d0694ad2ec2cad43415a86305c8671bd62833f11
swampservers <swampservers@gmail.com>
2019-04-18 13:47:25 -0700

    Update shared.lua
    Ogg

--- lua/entities/gmt_instrument_base/shared.lua
+++ lua/entities/gmt_instrument_base/shared.lua
@@ -64,7 +64,7 @@ function ENT:GetSound( snd )
 		return nil
 	end
 	
-	return "gmodtower/lobby/instruments/" .. (self.Instruments[self:GetNWInt("CurrentInstrument")] or "Piano") .. "/" .. snd .. ".wav"
+	return "gmodtower/lobby/instruments/" .. (self.Instruments[self:GetNWInt("CurrentInstrument")] or "Piano") .. "/" .. snd .. ".ogg"
 end
 
 if SERVER then
