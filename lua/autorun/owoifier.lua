067bdc16a43e167bf4be50ca61e2244199b8b592
Chev <11602755+chev2@users.noreply.github.com>
2019-07-09 01:02:45 -0700

    owoifier changes
    Emotes/chatcolors work properly now, and some other things

--- lua/autorun/owoifier.lua
+++ lua/autorun/owoifier.lua
@@ -1,29 +1,52 @@
-local OwOemotes = {"OwO", "owo", "UwU", "uwu", "TwT", ">w<", ":3c", ":3", ":33", ":333", "nya", "(> owo )>",
-					"*pounces on you*", "*glomps u*", "mrowww", "~", ":owo:", ":uwu:", ":awoo:", ":blush:", 
-					":catgirl:", ":pat:", ":snug:", ":thinkerfelix:", ":weebblush1:", ":weebears:", ":widetrap:"} //Add more emotes here
+local OwOemotes = {"OwO", "owo", "UwU", "uwu", "TwT", ">w<", ">wO", "Ow<", ":3c", ":3", ":33", ":333", "nya", "(> owo )>",
+					"(> owo <)", "*pounces on you*", "*glomps u*", "mrowww", "~", ":owo:", ":uwu:", ":awoo:", ":blush:", 
+					":catgirl:", ":pat:", ":snug:", ":thinkerfelix:", ":weebblush1:", ":weebears:", ":widetrap:"} --Add more emotes here
 
 local function OwOifier(text)
-	local text = string.Replace(string.Replace(text,"R","W"),"r","w")
-	local text = string.Replace(string.Replace(text,"L","W"),"l","w")
+	local splitstring = string.Split(text, "")
+	local isemoteenabled = false
 
-	for emotecolon in string.gmatch(text, ":%a-:") do //replace swampchat image emotes with text ones, because I cant figure out how to prevent them from being owo-ified
-		text = string.gsub(text, emotecolon, OwOemotes[math.random(#OwOemotes)])
+	for k, v in pairs(splitstring) do
+		if v == ":" or v == ";" or v == "[" or v == "]" then --Keep emotes and chat colors the same
+			isemoteenabled = !isemoteenabled
+		elseif v == " " then
+			isemoteenabled = false
+		end
+		if !isemoteenabled then
+			if splitstring[k+1] then
+				local dtext = table.concat(splitstring, "", k, k+1)
+
+				for nyaaa in string.gmatch(dtext, "n[aeiou]+") do --n(vowel) to ny(vowel)
+					local ny = "ny"..string.TrimLeft(nyaaa, "n")
+					local jointext = string.gsub(dtext, nyaaa, ny)
+					table.remove(splitstring, k+1)
+					table.remove(splitstring, k)
+					table.insert(splitstring, k, jointext)
+				end
+			end
+
+			if string.find(v, "[RrLl]") then --R and L to W
+				local stext = string.Replace(string.Replace(v, "R", "W"), "r", "w")
+				local stext = string.Replace(string.Replace(stext, "L", "W"), "l", "w")
+				table.remove(splitstring, k)
+				table.insert(splitstring, k, stext)
+			end		
+		end
 	end
-	for emotesemicolon in string.gmatch(text, ";%a-;") do
-		text = string.gsub(text, emotesemicolon, OwOemotes[math.random(#OwOemotes)])
+
+	local owotext = table.concat(splitstring)
+
+	local owotext = string.gsub(owotext, "this", "dis")
+
+	if string.StartWith(owotext, "!") then
+		owotext = string.gsub(owotext, "!woww", "!roll") --revert some commands
+		owotext = string.gsub(owotext, "!pwaytime", "!playtime")
+		owotext = string.gsub(owotext, "!kiwws", "!kills")
 	end
 
-	for nyaaa in string.gmatch(text, "n[aeiou]+") do //n(vowel) to ny(vowel)
-		local ny = "ny"..string.TrimLeft(nyaaa, "n")
-		text = string.gsub(text, nyaaa, ny)
-	end
+	local owotext = string.format(owotext.." %s", OwOemotes[math.random(#OwOemotes)]) --add a random emote at the end
 
-	local text = string.gsub(text, "this", "dis")
-	local text = string.gsub(text, "!woww", "!roll") //revert !roll
-
-	local text = string.format(text.." %s", OwOemotes[math.random(#OwOemotes)]) //add a random emote at the end
-
-	return text
+	return owotext
 end
 
 if SERVER then
@@ -37,31 +60,15 @@ if SERVER then
 
 	hook.Add("PlayerSay", "ClientOwOToggle", function(ply, text, team)
 		if string.lower(text) == "!owo" then
-			if ply:GetNWBool("OwOEnabled", false) == false then //Enable OwO
+			if ply:GetNWBool("OwOEnabled", false) == false then --Enable OwO
 				ply:SetNWBool("OwOEnabled", true)
-				net.Start("OwOSendToggleInfo")
-					net.WriteEntity(ply)
-					net.Send(ply)
+				ply:ChatPrint("[pink]owo enabled")
 				return ""
-			elseif ply:GetNWBool("OwOEnabled", false) == true then //Disable OwO
+			elseif ply:GetNWBool("OwOEnabled", false) == true then --Disable OwO
 				ply:SetNWBool("OwOEnabled", false)
-				net.Start("OwOSendToggleInfo")
-					net.WriteEntity(ply)
-					net.Send(ply)
+				ply:ChatPrint("[piunk]owo disabled")
 				return ""
 			else end
 		end
-	end)
-end
-
-if CLIENT then
-	local owochatcolor = Color(202, 67, 247)
-	net.Receive("OwOSendToggleInfo",function()
-		local ply = net.ReadEntity()
-		if ply:GetNWBool("OwOEnabled", false) == false then
-			chat.AddText(owochatcolor, "owo enabled")
-		elseif ply:GetNWBool("OwOEnabled", false) == true then
-			chat.AddText(owochatcolor, "owo disabled")
-		else end
 	end)
 end
