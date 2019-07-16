abab2aa435d6091d009fe715b3d00a61f8f10cd3
NottyNoz <redmathred@gmail.com>
2019-07-16 19:40:28 -0400

    bounty change 6

--- lua/autorun/server/bounties.lua
+++ lua/autorun/server/bounties.lua
@@ -1,8 +1,12 @@
 
+BountyLimit = {}
+
 hook.Add("PlayerDeath","BountyDeath",function(ply,infl,atk)
 	local bounty = GetPlayerBounty(ply)
+	if BountyLimit[ply:SteamID()] == nil then BountyLimit[ply:SteamID()] = 0 end
 	if bounty > 0 and ply != atk and atk:IsPlayer() then
 		SetPlayerBounty(ply,0)
+		BountyLimit[ply:SteamID()] = math.Clamp(BountyLimit[ply:SteamID()] - bounty,0,10000000)
 		atk:PS_GivePoints(bounty)
 		BotSayGlobal("[edgy]"..atk:Nick().." [fbc]has claimed [gold]"..ply:Nick().."'s [fbc]bounty of [rainbow]"..bounty.." [fbc]points!")
 	end
@@ -23,10 +27,16 @@ end
 function AddBounty(ply,target,amount)
 	if !ply:IsPlayer() or !target:IsPlayer() or type(amount) != "number" then return end
 	amount = amount > 0 and amount or 0
-	if ply:PS_HasPoints(amount) then
-		SetPlayerBounty(target,GetPlayerBounty(target) + amount)
+	if BountyLimit[ply:SteamID()] == nil then BountyLimit[ply:SteamID()] = 0 end
+	local total = BountyLimit[ply:SteamID()] + amount
+	if ply:PS_HasPoints(amount) and total <= 10000000 then
+		local add = GetPlayerBounty(target) + amount
+		BountyLimit[ply:SteamID()] = BountyLimit[ply:SteamID()] + amount
+		SetPlayerBounty(target,add)
 		ply:PS_TakePoints(amount)
-		BotSayGlobal("[fbc]"..target:Nick().."'s bounty is now [rainbow]"..GetPlayerBounty(target) + amount.." [fbc]points")
+		BotSayGlobal("[fbc]"..target:Nick().."'s bounty is now [rainbow]"..add.." [fbc]points")
+	elseif ply:PS_HasPoints(amount) and total > 10000000 then
+		ply:ChatPrint("[red]You have reached your limit for today")
 	else
 		ply:ChatPrint("[red]You don't have enough points")
 	end
@@ -34,27 +44,39 @@ end
 
 RegisterChatCommand({'bounty','setbounty'},function(ply,arg)
 	local t = string.Explode(" ",arg)
-	local to,c = PlyCount(t[1])
+	local to,c = PlyCount(string.Implode(table.remove(t)))
 	if to:IsPlayer() and c == 1 then
 		local bounty = GetPlayerBounty(to)
-		local p = tonumber(t[2])
+		local p = tonumber(t[#t])
 		if p != nil then
 			if p >= 1000 then
 				AddBounty(ply,to,p)
 			else
 				ply:ChatPrint("[red]You must add a minimum of 1000 points to the bounty")
 			end
-		elseif bounty > 0 then
-			BotSayGlobal(to:Nick().."'s bounty is [rainbow]"..bounty.." [fbc]points")
-		elseif bounty == 0 then
-			BotSayGlobal(to:Nick().." has no bounty")
 		end
 	elseif c>1 then
-		ply:ChatPrint("[fbc]More than one person found with that string in their name")
+		ply:ChatPrint("[red]More than one person found with that string in their name")
 	else
-		ply:ChatPrint("[fbc]!bounty player points")
+		ply:ChatPrint("[orange]!bounty player points")
 	end
 end,{global=true,throttle=true})
+
+RegisterChatCommand({'showbounty'},function(ply,arg)
+	local to,c = PlyCount(arg)
+	if to:IsPlayer() and c == 1 then
+		local bounty = GetPlayerBounty(to)
+		if bounty > 0 then
+			ply:ChatPrint("[orange]"..to:Nick().."'s bounty is [edgy]"..bounty.." [orange]points")
+		else
+			ply:ChatPrint("[orange]"..to:Nick().." has no bounty")
+		end
+	elseif c>1 then
+		ply:ChatPrint("[red]More than one person found with that string in their name")
+	else
+		ply:ChatPrint("[orange]!showbounty player")
+	end
+end,{global=false,throttle=false})
 
 RegisterChatCommand({'bounties','showbounties'},function(ply,arg)
 	local t = {}
@@ -66,7 +88,7 @@ RegisterChatCommand({'bounties','showbounties'},function(ply,arg)
 	end
 	table.sort(t,function(a,b) return a[2] > b[2] end)
 	ply:ChatPrint("[fbc]--- [gold]Bounties [fbc]---")
-	for k,v in pairs(t) do
+	for k,v in ipairs(t) do
 		if k <= 10 then
 			ply:ChatPrint(v[1]:Nick()..": "..v[2])
 		end
