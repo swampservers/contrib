59959bead0a9bf92cf88d8b0f87700e845e11449
Chev <11602755+chev2@users.noreply.github.com>
2019-07-19 23:56:20 -0700

    Dodgeball NULL Entity Fix

--- lua/weapons/weapon_dodgeball/shared.lua
+++ lua/weapons/weapon_dodgeball/shared.lua
@@ -11,19 +11,19 @@ local innie = 28
 function SWEP:PrimaryAttack()
 	self:SetNextPrimaryFire(CurTime() + 1)
 	self.Owner:SetAnimation( PLAYER_ATTACK1 )
-if SERVER then
-	timer.Simple(.1, function()
-		if self and self.Owner then
+	if SERVER then
+		timer.Simple(.1, function()
+			if IsValid(self) and IsValid(self.Owner) then
 				local p1 = self.Owner:GetPos()+self.Owner:GetCurrentViewOffset()
 				local p2 = p1+(self.Owner:GetAimVector()*outie)
 				local tr = util.TraceLine({ start=p1, endpos=p2, mask=MASK_SOLID_BRUSHONLY } )
 				if tr.Hit then p2 = tr.HitPos end
 				p2 = p2-(self.Owner:GetAimVector()*innie)
-	makeDodgeball(p2,(self.Owner:GetAimVector()*1000)+self.Owner:GetVelocity(),self.Owner)
-	self.Owner:StripWeapon("weapon_dodgeball")
-		end
-	end)
-end
+				makeDodgeball(p2,(self.Owner:GetAimVector()*1000)+self.Owner:GetVelocity(),self.Owner)
+				self.Owner:StripWeapon("weapon_dodgeball")
+			end
+		end)
+	end
 end
 
 function SWEP:SecondaryAttack()
