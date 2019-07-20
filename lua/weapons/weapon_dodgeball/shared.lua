f6a89a88196852efc39d1b83d46f5769cd35d7c9
Chev <11602755+chev2@users.noreply.github.com>
2019-07-20 01:12:03 -0700

    Dodgeball NULL Entity Fix 2

--- lua/weapons/weapon_dodgeball/shared.lua
+++ lua/weapons/weapon_dodgeball/shared.lua
@@ -31,14 +31,14 @@ self:SetNextSecondaryFire(CurTime() + 1)
 self.Owner:SetAnimation( PLAYER_ATTACK1 )
 if SERVER then
 	timer.Simple(.1, function()
-		if self and self.Owner then
-				local p1 = self.Owner:GetPos()+self.Owner:GetCurrentViewOffset()
-				local p2 = p1+(self.Owner:GetAimVector()*outie)
-				local tr = util.TraceLine({ start=p1, endpos=p2, mask=MASK_SOLID_BRUSHONLY } )
-				if tr.Hit then p2 = tr.HitPos end
-				p2 = p2-(self.Owner:GetAimVector()*innie)
-	makeDodgeball(p2,(self.Owner:GetAimVector()*500)+self.Owner:GetVelocity(),self.Owner)
-	self.Owner:StripWeapon("weapon_dodgeball")
+		if IsValid(self) and IsValid(self.Owner) then
+			local p1 = self.Owner:GetPos()+self.Owner:GetCurrentViewOffset()
+			local p2 = p1+(self.Owner:GetAimVector()*outie)
+			local tr = util.TraceLine({ start=p1, endpos=p2, mask=MASK_SOLID_BRUSHONLY } )
+			if tr.Hit then p2 = tr.HitPos end
+			p2 = p2-(self.Owner:GetAimVector()*innie)
+			makeDodgeball(p2,(self.Owner:GetAimVector()*500)+self.Owner:GetVelocity(),self.Owner)
+			self.Owner:StripWeapon("weapon_dodgeball")
 		end
 	end)
 end
@@ -49,14 +49,14 @@ function SWEP:Reload()
 	self.Owner:SetAnimation( PLAYER_ATTACK1 )
 if SERVER then
 	timer.Simple(.05, function()
-		if self and self.Owner then
-				local p1 = self.Owner:GetPos()+self.Owner:GetCurrentViewOffset()
-				local p2 = p1+(self.Owner:GetAimVector()*outie)
-				local tr = util.TraceLine({ start=p1, endpos=p2, mask=MASK_SOLID_BRUSHONLY } )
-				if tr.Hit then p2 = tr.HitPos end
-				p2 = p2-(self.Owner:GetAimVector()*innie)
-	makeDodgeball(p2,(self.Owner:GetAimVector()*200)+self.Owner:GetVelocity(),self.Owner)
-	self.Owner:StripWeapon("weapon_dodgeball")
+		if IsValid(self) and IsValid(self.Owner) then
+			local p1 = self.Owner:GetPos()+self.Owner:GetCurrentViewOffset()
+			local p2 = p1+(self.Owner:GetAimVector()*outie)
+			local tr = util.TraceLine({ start=p1, endpos=p2, mask=MASK_SOLID_BRUSHONLY } )
+			if tr.Hit then p2 = tr.HitPos end
+			p2 = p2-(self.Owner:GetAimVector()*innie)
+			makeDodgeball(p2,(self.Owner:GetAimVector()*200)+self.Owner:GetVelocity(),self.Owner)
+			self.Owner:StripWeapon("weapon_dodgeball")
 		end
 	end)
 end
