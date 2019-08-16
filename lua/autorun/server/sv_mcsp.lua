4bdd9e83fb254d50f61fe381438b40085513cee6
Chev <11602755+chev2@users.noreply.github.com>
2019-08-16 16:03:38 -0700

    Update with PData

--- lua/autorun/server/sv_mcsp.lua
+++ lua/autorun/server/sv_mcsp.lua
@@ -1,17 +1,22 @@
 local MCSPmodel = "models/milaco/minecraft_pm/minecraft_pm.mdl"
 
+local function MinecraftUpdateURL(ply, url)
+	if (ply.SetMCSPTimeout or 0) > CurTime() - 10 then ply:Notify("Cooldown...") return end --cooldown
+	ply.SetMCSPTimeout = CurTime()
+
+	if ply:GetModel() != MCSPmodel then return end
+	local url = SanitizeImgurId(url)
+	if !url then return ply:ChatPrint("[red]Invalid URL!") end
+
+	ply:SetNWString("MCSPSkinURL", url)
+	ply:SetPData("MinecraftURL", url)
+end
+
 RegisterChatCommand({'minecraftskin','skinpicker','minecrafturl'}, function(ply, arg)
 	if ply:GetModel() == MCSPmodel then
 		local argtable = string.Explode(" ", arg)
 		if argtable[1] then
-			if (ply.SetMCSPTimeout or 0) > CurTime() - 10 then ply:Notify("Cooldown...") return end --cooldown
-			ply.SetMCSPTimeout = CurTime()
-
-			if ply:GetModel() != MCSPmodel then return end
-			local url = SanitizeImgurId(argtable[1])
-			if !url then return ply:ChatPrint("[red]Invalid URL!") end
-
-			ply:SetNWString("MCSPSkinURL", url)
+			MinecraftUpdateURL(ply, argtable[1])
 		else
 			ply:ChatPrint("[orange]Usage: !minecraftskin (url)")
 		end
@@ -22,4 +27,9 @@ end, {global=false, throttle=true})
 
 RegisterChatCommand({'mcreset','minecraftreset','minecraftskinreset'}, function(ply, arg)
 	ply:SetNWString("MCSPSkinURL", false)
+	ply:RemovePData("MinecraftURL")
 end, {global=false, throttle=true})
+
+hook.Add("PlayerInitialSpawn", "MinecraftFindURL", function(ply)
+	if ply:GetModel() == MCSPmodel and ply:GetPData("MinecraftURL", false) then MinecraftUpdateURL(ply, ply:GetPData("MinecraftURL", false)) end
+end)
