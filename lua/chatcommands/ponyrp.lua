9476621e1c003aae786651f3dce95c0b61c0b293
Chev <11602755+chev2@users.noreply.github.com>
2019-08-23 17:40:45 -0700

    pony RP command

---
+++ lua/chatcommands/ponyrp.lua
@@ -0,0 +1,18 @@
+RegisterChatCommand({'ponyrp'}, function(ply, arg)
+	if !Safe(ply) and !ply:InVehicle() then
+		if !IsValid(ply) then return end
+		ply:SetPos(Vector(-388, 1400, -118)) --treatment room location
+
+		for k, v in pairs(ents.GetAll()) do
+			if !IsValid(v) then end
+			if v:GetName() == "treatmentdoor" then v:Fire("Close") end
+			if v:GetName() == "treatmentlever" then v:Fire("PressOut") end
+		end
+	end
+end, {global=false, throttle=true})
+
+hook.Add("PlayerSay", "TreatmentRoomChat", function(ply, text, team)
+	if ply:GetLocationName() == "Treatment Room" then
+		return "i like ponies"
+	end
+end)
