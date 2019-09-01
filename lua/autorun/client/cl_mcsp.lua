08593cf30a755d67dbce70bce64ea1a3333cbd48
Chev <11602755+chev2@users.noreply.github.com>
2019-09-01 03:04:45 -0700

    Minecraft lua + some updates to milaco's materials (#64)
    * Base uses toolsblack/Minecraft PM is Steve
    
    * Clientside MCSP initial commit
    
    * Serverside MCSP initial commit
    
    * Register minecraft model in pointshop
    
    * Replace hook chat commands with register chat commands
    
    * fix indent
    
    * Add cooldown to skin update
    
    * fix nil value
    
    * Use ImgurMaterial/Replace SetSubMaterial with render.MaterialOverride()
    
    * Remove getstring
    
    * use MaterialOverrideByIndex to accommodate for transparent textures
    
    * Update ragdoll to use MaterialOverride
    
    * Use ply value instead of NWString
    
    * Clientside changes
    
    * Serverside changes
    
    * Update with PData
    
    * Change ply to v
    
    * Minecraft derma string request in pointshop
    
    * Remove old materials
    
    * Update minecraft materials
    
    * Update minecraft lua
    
    * Add proper resetting for base layer
    
    * Change PlayerInitialSpawn
    
    * CreateClientsideRagdoll
    
    * NWString check
    
    * Update material files
    
    * Change $alpha to $opaque & add back hat layer
    
    * Fix LocalPlayer()
    
    * fix typo

---
+++ lua/autorun/client/cl_mcsp.lua
@@ -0,0 +1,38 @@
+local MCSPmodel = "models/milaco/minecraft_pm/minecraft_pm.mdl"
+
+hook.Add("PrePlayerDraw", "MCSPRenderSkin", function(ply) --Material ID 0 is the hat layer, ID 1 is the base layer
+	if IsValid(ply) and ply:GetModel() == MCSPmodel and ply:IsPlayer() and ply:GetNWString("MCSPSkinURL", false) then
+		render.MaterialOverrideByIndex(1, ImgurMaterial(ply:GetNWString("MCSPSkinURL", false), ply, ply:GetPos(), false, "VertexLitGeneric", {
+			["$opaque"] = 1,
+			["$halflambert"] = 1,
+			["$model"] = 1
+		}))
+		render.MaterialOverrideByIndex(0, ImgurMaterial(ply:GetNWString("MCSPSkinURL", false), ply, ply:GetPos(), false, "VertexLitGeneric", {
+			["$alphatest"] = 1,
+			["$halflambert"] = 1,
+			["$model"] = 1
+		}))
+	end
+end)
+
+hook.Add("PostPlayerDraw", "MCSPRenderSkin", function(ply)
+	if IsValid(ply) and ply:GetModel() == MCSPmodel and ply:IsPlayer() then
+		render.MaterialOverrideByIndex(0, "")
+		render.MaterialOverrideByIndex(1, "")
+	end
+end)
+
+hook.Add("CreateClientsideRagdoll", "DrawMinecraftPlayerRagdolls", function(ent, ragdoll)
+	if IsValid(ent) and IsValid(ragdoll) and ragdoll:GetModel() == MCSPmodel and ent:GetNWString("MCSPSkinURL", false) then
+		ragdoll:SetSubMaterial(1, "!"..ImgurMaterial(ent:GetNWString("MCSPSkinURL", false), ent, ent:GetPos(), false, "VertexLitGeneric", {
+			["$opaque"] = 1,
+			["$halflambert"] = 1,
+			["$model"] = 1
+		}):GetName())
+		ragdoll:SetSubMaterial(0, "!"..ImgurMaterial(ent:GetNWString("MCSPSkinURL", false), ent, ent:GetPos(), false, "VertexLitGeneric", {
+			["$alphatest"] = 1,
+			["$halflambert"] = 1,
+			["$model"] = 1
+		}):GetName())
+	end
+end)
