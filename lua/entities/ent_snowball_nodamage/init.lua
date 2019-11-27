39645f64aa593046f17841bbcc2a73d364705910
Chev <11602755+chev2@users.noreply.github.com>
2019-11-26 18:51:21 -0800

    Snowball SWEP (#94)
    * Snowball SWEP
    
    * Snowball entity
    
    * Snowball sounds
    
    * Snowball models
    
    * Snowball materials
    
    * Remove debug code
    
    * Replace self.Owner.currentcolor with a NWVector
    
    * Change the crowbar sound to be native

---
+++ lua/entities/ent_snowball_nodamage/init.lua
@@ -0,0 +1,40 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+
+local hitsnd = Sound("weapons/weapon_snowball/snowhit.ogg")
+
+function ENT:Initialize()
+	self.Entity:SetModel(self.Model)
+	self.Entity:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+	self.Entity:SetSolid(SOLID_VPHYSICS)
+	self:PhysicsInitSphere(10, "ice")
+
+	local phys = self:GetPhysicsObject()
+	if phys:IsValid() then
+		phys:Wake()
+		phys:EnableGravity(true)
+	end
+
+	local plycol = self.Owner:GetNWVector("SnowballColor", Vector(1, 1, 1)):ToColor()
+
+	self.Trail = util.SpriteTrail(self.Entity, 0, plycol, false, 15, 0, 0.8, 1/(15+0)*0.5, "trails/laser.vmt") --color trail
+end
+
+function ENT:PhysicsCollide(data)
+	local pos = self.Entity:GetPos()
+	local effectdata = EffectData()
+	local fwd = self:GetOwner():GetAimVector()
+	if data.HitEntity:IsPlayer() then --push players around a small amount
+		data.HitEntity:SetVelocity(Vector(fwd * 100))
+	end
+
+	effectdata:SetOrigin(pos)
+	effectdata:SetScale(1.5)
+	self:EmitSound(hitsnd)
+	util.Effect("WheelDust", effectdata)
+	util.Effect("GlassImpact", effectdata)
+	SafeRemoveEntityDelayed(self, 0.01)
+end 
---
+++ lua/entities/ent_snowball_nodamage/shared.lua
@@ -0,0 +1,4 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.Model = Model("models/weapons/w_snowball_thrown.mdl")
