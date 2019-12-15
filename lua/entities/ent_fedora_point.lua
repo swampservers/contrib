ce35ae0dfcf4bb36539468c2ed3133d400a69d4b
Chev <11602755+chev2@users.noreply.github.com>
2019-12-15 10:45:36 -0800

    Flappy entity (to tie the trail to)

---
+++ lua/entities/ent_fedora_point.lua
@@ -0,0 +1,34 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+DEFINE_BASECLASS("base_gmodentity")
+
+function ENT:Initialize()
+	if CLIENT then
+		self.Entity:SetNoDraw(true)
+	else
+		self.Trail = util.SpriteTrail(self, 0, Color(255, 255, 255), false, 4, 4, 0.2, 1/8, "chev/rainbowdashtrail") --trail is parented to this entity
+	end
+end
+
+function ENT:Think()
+	if SERVER then return end
+	local ply = self:GetOwner()
+
+	if !IsValid(ply) then return end
+	if (ply == LocalPlayer() and THIRDPERSON) or ply != LocalPlayer() then --only move the entity if in thirdperson, or the player isn't the localplayer
+		local bn = ply:IsPony() and "LrigSpine1" or "ValveBiped.Bip01_Head1"
+		local bon = ply:LookupBone(bn) or 0
+
+		local bonepos = ply:GetBonePosition(bon)
+		local plyaim = ply:GetAimVector()
+
+		if ply:IsPony() then
+			self:SetPos(bonepos + Vector(plyaim.x * 1, plyaim.y * 1, plyaim.z - 4))
+		else
+			self:SetPos(bonepos + Vector(plyaim.x * 2, plyaim.y * 2, plyaim.z+7))
+		end
+	end
+end
+
+function ENT:Draw() end
