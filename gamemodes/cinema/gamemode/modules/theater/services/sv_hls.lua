625d19e78b6c857d83b0c171ae2d03529a2fa31c
NottyNoz <redmathred@gmail.com>
2020-03-16 16:45:09 -0400

    hls change

--- gamemodes/cinema/gamemode/modules/theater/services/sv_hls.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sv_hls.lua
@@ -1,18 +1,43 @@
+
 sv_GetVideoInfo = sv_GetVideoInfo or {}
 
 sv_GetVideoInfo.hls = function(self, key, ply, onSuccess, onFailure)
-	local onReceive = function(info)
 
+	local match = string.match(key,"streamwat.ch/(%w+)/*$")
+	
+	local onReceive = function(info)
+	
 		http.Post( "https://swampservers.net/fedorabot/gis.php", {q=info.title}, 
 			function(body) 
-				info.thumb="http://swampservers.net/cinema/contain.php?i="..body 
+				info.thumb="http://swampservers.net/cinema/contain.php?i="..body
 				onSuccess(info)
 			end, onFailure)
-	end
-
-	theater.GetVideoInfoClientside(self:GetClass(), key, ply, onReceive, onFailure)
-end
-
-
-
 			
+	end
+	
+	local onFetchReceive = function( body, length, headers, code )
+		
+		local match2 = string.match(body,"(http.+%.m3u8)")
+		
+		if match2 != nil then
+		
+			theater.GetVideoInfoClientside(self:GetClass(), key, ply, function(info)
+				http.Post( "https://swampservers.net/fedorabot/gis.php", {q=info.title}, 
+					function(body2) 
+						info.thumb="http://swampservers.net/cinema/contain.php?i="..body2
+						info.data = match2
+						onSuccess(info)
+					end, onFailure)
+			end, onFailure)
+		
+		end
+		
+		onFailure( 'Theater_RequestFailed' )
+	end
+	
+	if match != nil then
+		self:Fetch( "http://streamwat.ch/"..match.."/player.min.js", onFetchReceive, onFailure )
+	else
+		theater.GetVideoInfoClientside(self:GetClass(), key, ply, onReceive, onFailure)
+	end
+end
