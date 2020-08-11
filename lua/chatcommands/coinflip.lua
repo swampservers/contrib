b829a257d71b95f7b3854efdf9b9c8912fde45e4
NottyNoz <redmathred@gmail.com>
2020-08-10 23:11:19 -0400

    coinflip fix

--- lua/chatcommands/coinflip.lua
+++ lua/chatcommands/coinflip.lua
@@ -19,10 +19,10 @@ RegisterChatCommand({'coin','coinflip'},function(ply,arg)
 			elseif p >= 1000 then -- minimum 1000 point coinflip
 				initCoinFlip(ply,to,p)
 			else
-				ply:ChatPrint("[red]A coin flip must be a minimum of 1000 points. No pussy bets!")
+				ply:ChatPrint("[red]A coin flip must be a minimum of 1000 points.")
 			end
 		else
-			ply:ChatPrint("[red]Player "..string.Implode(" ",t).." not found")
+			ply:ChatPrint("[red]Player "..string.Implode(" ",t).." not found.")
 		end
 	end
 end,{global=true,throttle=true})
@@ -36,12 +36,12 @@ function()
 			local fromPlayer = player.GetBySteamID(fromID)
 			local toPlayer = player.GetBySteamID(coinflip[1])
 			if(fromPlayer ~= nil and toPlayer ~= nil) then -- This whole nonsense is because I want to show the from/to's name if possible, but otherwise show a different message.
-				fromPlayer:ChatPrint("[edgy]" .. toPlayer:Nick() .. "[fbc] don't want to play. Try again lates.")
+				fromPlayer:ChatPrint("[edgy]" .. toPlayer:Nick() .. "[fbc] doesn't want to play. Try again later.")
 				toPlayer:ChatPrint("[fbc]You missed out on a coinflip from [edgy]" .. fromPlayer:Nick() .. "[fbc].")
 			elseif(fromPlayer == nil and toPlayer ~= nil) then
 				toPlayer:ChatPrint("[fbc]You missed out on a coinflip.")
 			elseif(fromPlayer ~= nil and toPlayer == nil) then
-				fromPlayer:ChatPrint("[fbc]The player you requested a coinflip to has left. Try again lates.")
+				fromPlayer:ChatPrint("[fbc]The player you requested a coinflip to has left. Try again later.")
 			end
 		else
 			NewCoinFlips[fromID] = coinflip
@@ -52,12 +52,12 @@ end)
 
 function initCoinFlip(ply,target,amount)
 	if ply:PS_HasPoints(amount) and CoinFlips[ply:SteamID()] == nil then
-		ply:ChatPrint("[orange]"..to:Nick().." is receiving your coinflip request.")
+		ply:ChatPrint("[orange]"..target:Nick().." is receiving your coinflip request.")
 		CoinFlips[ply:SteamID()] = {target:SteamID(), amount, CurTime()}
 	elseif CoinFlips[ply:SteamID()] ~= nil then
-		ply:ChatPrint("[red]What the hell r yeh doing bruv? yeh already hae a filp goin!")
+		ply:ChatPrint("[red]You already have a coinflip in progress.")
 	elseif not ply:PS_HasPoints(amount) then
-		ply:ChatPrint("[red]What the hell r yeh doing bruv? yeh don't hae dooze points!")
+		ply:ChatPrint("[red]You don't have enough points.")
 	end
 end
 
@@ -77,7 +77,7 @@ function checkCoinFlipRequest(toPlayer, points)
 		end
 	end
 	if not coinflipFound then
-		ply:ChatPrint("[red]Yeh don't hae a coinflip request for tha amount!")
+		ply:ChatPrint("[red]You don't have a coinflip request for that amount!")
 		ply:ChatPrint("[orange]COINFLIPS:")
 		local index = 1
 		for fromID,j in pairs(CoinFlips) do
@@ -85,7 +85,7 @@ function checkCoinFlipRequest(toPlayer, points)
 				local fromPlayer =  player.GetBySteamID(fromID)
 				local points = j[2]
 				if fromPlayer ~= nil then
-					ply:ChatPrint("[orange]" .. index .. ") [rainbow]" .. points .. "[fbc] from [gold]" .. fromPlayer:Nick())
+					ply:ChatPrint("[orange](" .. index .. ") [rainbow]" .. points .. "[fbc] from [gold]" .. fromPlayer:Nick())
 				end
 				index = index + 1
 			end
@@ -102,13 +102,13 @@ function finishCoinFlip(fromID, toPlayer)
 	end
 	if(fromPlayer == nil) then
 		CoinFlipRemove(fromID) -- Remove request from CoinFlip because initiator left the server
-		toPlayer:ChatPrint("[red]The other bruv left, gotta cancel this coinflip!")
+		toPlayer:ChatPrint("[red]The initiator left, coinflip cancelled.")
 	elseif fromPlayer:PS_HasPoints(amount) and toPlayer:PS_HasPoints(amount) then -- Final Check, make sure they have funds still
 		CoinFlipRemove(fromID)
 		local heads = math.random() < 0.5 -- the "request from" player is always Heads.
-		BotSayGlobal("[edgy]" .. fromPlayer:Nick() .. "[fbc] flipped a coin worth [rainbow]" .. (amount * 2) .. "[fbc] against [gold]".. toPlayer:Nick().. "[fbc] and...... [rainbow]" .. (heads and "Won" or "Lost") .."!")
-		fromPlayer:ChatPrint("[fbc]You " .. (heads and "Won" or "Lost") .. " " .. amount .. ".")
-		toPlayer:ChatPrint("[fbc]You " .. (heads and "Won" or "Lost") .. " " .. amount .. ".")
+		BotSayGlobal("[edgy]" .. fromPlayer:Nick() .. "[fbc] flipped a coin worth [rainbow]" .. (amount * 2) .. "[fbc] against [gold]".. toPlayer:Nick().. "[fbc] and [rainbow]" .. (heads and "Won" or "Lost") .."!")
+		fromPlayer:ChatPrint("[fbc]You " .. (heads and "Won" or "Lost") .. " [gold]" .. amount .. "[fbc].")
+		toPlayer:ChatPrint("[fbc]You " .. (heads and "Won" or "Lost") .. " [gold]" .. amount .. "[fbc].")
 		-- Instead of taking the amount away from both and then giving the winner the amount x 2, simply remove/add here
 		if heads then
 			toPlayer:PS_TakePoints(amount)
