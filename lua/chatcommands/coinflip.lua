fff9beb6278d7be39cd52eb269b89a89c14dc162
NottyNoz <redmathred@gmail.com>
2020-08-11 21:15:14 -0400

    wtf

--- lua/chatcommands/coinflip.lua
+++ lua/chatcommands/coinflip.lua
@@ -32,17 +32,17 @@ function()
 	local NewCoinFlips = {}
 	for fromID,j in pairs(CoinFlips) do
 		if((j[3] + 20) <= CurTime()) then
-
 			local fromPlayer = player.GetBySteamID(fromID)
 			local toPlayer = player.GetBySteamID(j[1])
-			if(fromPlayer ~= nil and toPlayer ~= nil) then -- This whole nonsense is because I want to show the from/to's name if possible, but otherwise show a different message.
+			if(fromPlayer != nil and toPlayer != nil) then -- This whole nonsense is because I want to show the from/to's name if possible, but otherwise show a different message.
 				fromPlayer:ChatPrint("[edgy]" .. toPlayer:Nick() .. "[fbc] doesn't want to play. Try again later.")
 				toPlayer:ChatPrint("[fbc]You missed out on a coinflip from [edgy]" .. fromPlayer:Nick() .. "[fbc].")
-			elseif(fromPlayer == nil and toPlayer ~= nil) then
+			elseif(fromPlayer == nil and toPlayer != nil) then
 				toPlayer:ChatPrint("[fbc]You missed out on a coinflip.")
-			elseif(fromPlayer ~= nil and toPlayer == nil) then
+			elseif(fromPlayer != nil and toPlayer == nil) then
 				fromPlayer:ChatPrint("[fbc]The player you requested a coinflip to has left. Try again later.")
 			end
+			CoinFlips[fromID] = nil
 		else
 			NewCoinFlips[fromID] = j
 		end
@@ -54,16 +54,11 @@ function initCoinFlip(ply,target,amount)
 	if ply:PS_HasPoints(amount) and CoinFlips[ply:SteamID()] == nil then
 		ply:ChatPrint("[orange]"..target:Nick().." is receiving your coinflip request.")
 		CoinFlips[ply:SteamID()] = {target:SteamID(), amount, CurTime()}
-	elseif CoinFlips[ply:SteamID()] ~= nil then
+	elseif CoinFlips[ply:SteamID()] != nil then
 		ply:ChatPrint("[red]You already have a coinflip in progress.")
 	elseif not ply:PS_HasPoints(amount) then
 		ply:ChatPrint("[red]You don't have enough points.")
 	end
-end
-
-function CoinFlipRemove(id)
-	timer.Destroy("CoinFlip" .. id)
-	CoinFlips[id] = nil
 end
 
 function checkCoinFlipRequest(toPlayer, points)
@@ -73,7 +68,6 @@ function checkCoinFlipRequest(toPlayer, points)
 			-- Coinflip Request Found
 			coinflipFound = true
 			finishCoinFlip(fromID, toPlayer)
-			break -- Only do the first request found.
 		end
 	end
 	if not coinflipFound then
@@ -82,9 +76,9 @@ function checkCoinFlipRequest(toPlayer, points)
 		local index = 1
 		for fromID,j in pairs(CoinFlips) do
 			if j[1] == toPlayer:SteamID() then
-				local fromPlayer =  player.GetBySteamID(fromID)
+				local fromPlayer = player.GetBySteamID(fromID)
 				local points = j[2]
-				if fromPlayer ~= nil then
+				if fromPlayer != nil then
 					ply:ChatPrint("[fbc](" .. index .. ") [rainbow]" .. points .. "[fbc] from [gold]" .. fromPlayer:Nick())
 				end
 				index = index + 1
@@ -101,10 +95,10 @@ function finishCoinFlip(fromID, toPlayer)
 		if(ply:SteamID() == fromID) then fromPlayer = ply end
 	end
 	if(fromPlayer == nil) then
-		CoinFlipRemove(fromID) -- Remove request from CoinFlip because initiator left the server
+		CoinFlips[fromID] = nil -- Remove request from CoinFlip because initiator left the server
 		toPlayer:ChatPrint("[red]The initiator left, coinflip cancelled.")
 	elseif fromPlayer:PS_HasPoints(amount) and toPlayer:PS_HasPoints(amount) then -- Final Check, make sure they have funds still
-		CoinFlipRemove(fromID)
+		CoinFlips[fromID] = nil
 		local heads = math.random() < 0.5 -- the "request from" player is always Heads.
 		BotSayGlobal("[edgy]" .. fromPlayer:Nick() .. "[fbc] flipped a coin worth [rainbow]" .. (amount * 2) .. "[fbc] against [gold]".. toPlayer:Nick().. "[fbc] and [rainbow]" .. (heads and "Won" or "Lost") .."[fbc]!")
 		fromPlayer:ChatPrint("[fbc]You " .. (heads and "Won" or "Lost") .. " [gold]" .. amount .. "[fbc].")
@@ -118,7 +112,7 @@ function finishCoinFlip(fromID, toPlayer)
 			toPlayer:PS_GivePoints(amount)
 		end
 	else
-		CoinFlipRemove(fromID)
+		CoinFlips[fromID] = nil
 		toPlayer:ChatPrint("[red]ERROR: No points have been taken. Coinflip cancelled.")
 		fromPlayer:ChatPrint("[red]ERROR: No points have been taken. Coinflip cancelled.")
 	end
