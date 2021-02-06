6e00912c05fb82615783d3f888f6b7b1ff090328
NottyNoz <redmathred@gmail.com>
2021-02-05 21:06:21 -0500

    hls rework

--- gamemodes/cinema/gamemode/modules/theater/services/sv_dlive.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sv_dlive.lua
@@ -4,5 +4,25 @@
 sv_GetVideoInfo = sv_GetVideoInfo or {}
 
 sv_GetVideoInfo.dlive = function(self, key, ply, onSuccess, onFailure)
-	theater.GetVideoInfoClientside(self:GetClass(), key, ply, onSuccess, onFailure)
+	
+	local onFetchReceive = function( body, length, headers, code )
+		
+		local info = {}
+		info.title = string.match(body,'displayname":"(.+)","p') or "(Unknown)"
+		http.Fetch("https://live.prd.dlive.tv/hls/live/"..(string.match(body,'username":"(.+)","f') or "")..".m3u8",function(body2,length2,headers2,code2) --returns list of active streams
+			if (code2 == 200) then
+				for k,v in ipairs(string.Split(body2,"\n")) do
+					if (v:EndsWith("src/live.m3u8")) then
+						info.data = "https://cors.oak.re/"..v
+						info.duration = 0
+						onReceive(info)
+					end
+				end
+			end
+			onFailure( 'Theater_RequestFailed' )
+		end, onFailure)
+		
+	end
+	
+	self:Fetch( key, onFetchReceive, onFailure )
 end
--- gamemodes/cinema/gamemode/modules/theater/services/sv_hls.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sv_hls.lua
@@ -8,23 +8,51 @@ sv_GetVideoInfo.hls = function(self, key, ply, onSuccess, onFailure)
 	local streamwatch_key = string.match(key,"streamwat.ch/(%w+)/*$")
 	
 	local onReceive = function(info)
-	
+		
 		http.Post( "https://swampservers.net/fedorabot/gis.php", {q=info.title}, 
 			function(body) 
 				info.thumb="http://swampservers.net/cinema/contain.php?i="..body
 				onSuccess(info)
 			end, onFailure)
-			
+		
 	end
 	
 	local onFetchReceive = function( body, length, headers, code )
 		
+		local info = {}
+		local duration = 0
+		local timed = false
+		for k,v in ipairs(string.Split(body,"\n")) do
+			if (v:StartWith("#EXTINF:")) then
+				duration = duration+tonumber(string.Split(string.sub(v,9),",")[1]) --split because it can be 1.0000,live instead of just 10000,
+			end
+			if (v == "#EXT-X-ENDLIST") then
+				timed = true
+			end
+		end
+		if (string.Split(body,"\n")[1] == "#EXTM3U") then
+			theater.GetVideoInfoClientside(self:GetClass(), "TITLE", ply, function(info) --use player to get the title
+				info.duration = 0
+				if timed then
+					info.duration = math.ceil(duration)
+					info.data = "true"
+				end
+				onReceive(info)
+			end, onFailure)
+		end
+		onFailure( 'Theater_RequestFailed' )
+		
+	end
+	
+	local onFetchReceiveStreamWatch = function( body, length, headers, code )
+		
 		local streamwatch_url = string.match(body,"(http.+%.m3u8)")
 		
 		if streamwatch_url != nil or code == 0 then
-			theater.GetVideoInfoClientside(self:GetClass(), (code==0 and key) or streamwatch_url, ply, function(info)
+			theater.GetVideoInfoClientside(self:GetClass(), (code==0 and key) or streamwatch_url, ply, function(info) --use player to get the hls link due to serverside http issue
 				info.data = streamwatch_url
-				onSuccess(info)
+				info.duration = 0
+				onReceive(info)
 			end, onFailure)
 		else
 			onFailure( 'Theater_RequestFailed' )
@@ -33,8 +61,8 @@ sv_GetVideoInfo.hls = function(self, key, ply, onSuccess, onFailure)
 	end
 	
 	if streamwatch_key != nil then
-		self:Fetch( "https://streamwat.ch/"..streamwatch_key.."/player.min.js", onFetchReceive, onFailure )
+		self:Fetch( "https://streamwat.ch/"..streamwatch_key.."/player.min.js", onFetchReceiveStreamWatch, onFailure )
 	else
-		theater.GetVideoInfoClientside(self:GetClass(), key, ply, onReceive, onFailure)
+		self:Fetch( key, onFetchReceive, onFailure )
 	end
 end
