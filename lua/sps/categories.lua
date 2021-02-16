47b65913129725d08e6e4cd0286292cfefad02a3
PYROTEKNIK <explosionjuice@gmail.com>
2021-02-16 08:58:17 -0800

    Update categories.lua
    oops i forgot this shit

--- lua/sps/categories.lua
+++ lua/sps/categories.lua
@@ -15,6 +15,7 @@ PS_Categories = {
 					"weapon_anonymous",
 					"weapon_autism",
 					"weapon_funnybanana",
+					"weapon_monke",
 					"gmod_camera",
 					"weapon_switch",
 					"weapon_encyclopedia",
