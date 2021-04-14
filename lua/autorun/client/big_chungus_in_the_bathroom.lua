e9c4abf0c5b2687f785f6272797df0cf5de159a6
PYROTEKNIK <laynedelowsky@gmail.com>
2021-04-14 11:46:07 -0700

    cleaned up debug code

--- lua/autorun/client/big_chungus_in_the_bathroom.lua
+++ lua/autorun/client/big_chungus_in_the_bathroom.lua
@@ -2,7 +2,11 @@
 -- INSTALL: CINEMA
 BIG_CHUNGUS_LOCATION = Vector(0, 1598, 64)
 BIG_CHUNGUS_NORMAL = Vector(1, 0, 0)
+if (game.GetMap() ~= "cinema_swamp_v3") then return end
+if (engine.ActiveGamemode() ~= "cinema") then return end
 
+-- i don't know how badly i want to have to uninstall this every time i do something else
+-- i would put this in the map's location file but i don't have it. it should probably go there.
 function MAKE_CHUNGUS()
     if (not IsValid(CHUNGUS_ENTITY)) then
         CHUNGUS_ENTITY = ClientsideModel("models/hunter/blocks/cube025x025x025.mdl")
@@ -26,9 +30,9 @@ local function Chungus_Function(ply, key)
 end
 
 timer.Create("Chungus_Checker", 1, 0, function()
-    if (true or LocalPlayer().GetLocationName and LocalPlayer():GetLocationName() == "Bathroom") then
-        hook.Add("KeyPress", "Chungus", Chungus_Function)
+    if (LocalPlayer():GetLocationName() == "Bathroom") then
+        hook.Add("KeyPress", "ChungusBathroomKey", Chungus_Function)
     else
-        hook.Remove("KeyPress", "Chungus")
+        hook.Remove("KeyPress", "ChungusBathroomKey")
     end
 end)
