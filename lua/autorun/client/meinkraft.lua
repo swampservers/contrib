415bd0716dc45b704d0a0b27d7820233d062a57c
PYROTEKNIK <laynedelowsky@gmail.com>
2021-04-16 20:02:04 -0700

    no error on weird lights that don't have a dietime

--- lua/autorun/client/meinkraft.lua
+++ lua/autorun/client/meinkraft.lua
@@ -137,7 +137,7 @@ CVX_SORTABLE_DLIGHTS = {}
 
 DynamicLight = function(idx, elight)
     CVX_TRACKED_DLIGHTS[idx] = setmetatable({
-        _data = {},
+        _data = {dietime = 0},
         _light = BASE_DYNAMIC_LIGHT(idx, elight)
     }, CVX_DLIGHT_PROXY_META)
 
