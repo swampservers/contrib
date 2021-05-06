8e73b716077b4e9bcd8482dd80f82a031820d665
PYROTEKNIK <laynedelowsky@gmail.com>
2021-05-05 20:14:26 -0700

    Update swamp_core.lua
    oopsie

--- lua/autorun/swamp_core.lua
+++ lua/autorun/swamp_core.lua
@@ -57,12 +57,6 @@ function math.power2(n)
     return math.pow(2, math.ceil(math.log(n) / math.log(2)))
 end
 
-function vec:InBox( vec1, vec2 )
-	return self.x >= vec1.x && self.x <= vec2.x &&
-		self.y >= vec1.y && self.y <= vec2.y &&
-		self.z >= vec1.z && self.z <= vec2.z
-end
-
 BLACK = Color(0, 0, 0, 255)
 WHITE = Color(255, 255, 255, 255)
 
