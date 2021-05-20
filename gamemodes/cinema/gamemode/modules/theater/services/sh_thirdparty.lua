e1718d7a3d0c812ca3d5e8837020d07988b28879
NottyNoz <redmathred@gmail.com>
2021-05-20 16:58:56 -0400

    oops

--- gamemodes/cinema/gamemode/modules/theater/services/sh_thirdparty.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sh_thirdparty.lua
@@ -45,7 +45,7 @@ if CLIENT then
 
     function SERVICE:GetHost(Video)
         local t = util.JSONToTable(Video:Key())
-        return url.parse2(t).host
+        return url.parse2(t["key"]).host
     end
 
     function SERVICE:LoadVideo(Video, panel)
