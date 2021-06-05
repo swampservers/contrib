89ff0c087218791bd5ecc174bd9b77411f90407f
PYROTEKNIK <laynedelowsky@gmail.com>
2021-06-04 21:03:07 -0700

    Create elevator_patch.lua (#253)
    This should stop any moving sounds before the elevator starts moving.

---
+++ lua/autorun/server/elevator_patch.lua
@@ -0,0 +1,10 @@
+-- it'd probably be a good idea to look into why this is happening in the first place
+-- it seems like a fairly recent issue, but i'm not 100% positive
+
+hook.Add( "AcceptInput", "ElevatorSoundsFix", function( ent, name, activator, caller, data )
+    if ( ent:GetName() == "tt_elevator" ) then
+        if(name == "SetPosition")then
+            ent:StopSound("plats/elevator_move_loop1.wav")
+        end
+    end
+end )
