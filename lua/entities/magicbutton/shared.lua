26952cc1cead19ed47b58f4875507907a54d6045
PYROTEKNIK <laynedelowsky@gmail.com>
2021-06-09 03:02:02 -0700

    Update shared.lua

--- lua/entities/magicbutton/shared.lua
+++ lua/entities/magicbutton/shared.lua
@@ -4,8 +4,7 @@ AddCSLuaFile()
 ENT.Type = "anim"
 ENT.PrintName = "Secret Button"
 ENT.Author = "PYROTEKNIK"
-ENT.Purpose = "If you find one of these, something magic may happen"
 ENT.Category = "PYROTEKNIK"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
-MAGICBUTTON_TRANSMIT_DISTANCE = 3000
+HIDDENBUTTON_TRANSMIT_DISTANCE = 3000
