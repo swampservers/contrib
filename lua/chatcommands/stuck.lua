5dc06fbaf3496ea2bd6bc52ad15353e3b5a202d3
CaptainElderly <laynedelowsky@gmail.com>
2021-06-19 00:30:28 -0700

    Chat command that unsticks the player if they are stuck!

---
+++ lua/chatcommands/stuck.lua
@@ -0,0 +1,13 @@
+﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
+-- INSTALL: CINEMA
+
+
+
+RegisterChatCommand({'stuck'}, function(ply, arg)
+    if IsValid(ply) and not ply:InVehicle() and ply:Alive() then
+        ply:Unstick()
+    end
+end, {
+    global = false,
+    throttle = true
+})
