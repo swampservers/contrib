c655c1ae1c256e00c5cc28c8c4df597a4c0b59d3
CaptainElderly <laynedelowsky@gmail.com>
2021-06-20 02:58:55 -0700

    Battery % for HUD

--- lua/weapons/weapon_laserpointer.lua
+++ lua/weapons/weapon_laserpointer.lua
@@ -25,6 +25,13 @@ SWEP.DrawCrosshair = false
 SWEP.BounceWeaponIcon = false
 SWEP.LaserMask = nil --MASK_SHOT
 
+
+function SWEP:AmmoDisplayValue()
+    local psuf = "%"
+    local pval = math.Round((self:GetBattery() / self:GetFullBattery()) * 100, 0)
+    return pval..psuf
+end
+
 -- PlayerCanHaveLaserBeams is a hook used for this. the argument is just ply
 -- return false on this one and players won't be able to make their laser beam fully visible and lethal using right mouse
 hook.Add("PlayerCanHaveLaserBeams", "DisableBeamModeInTheaters", function(ply, wep)
