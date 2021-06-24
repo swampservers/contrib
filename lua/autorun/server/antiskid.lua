c25438b213c9a8659f1e5ca4503b311ab2ead774
Swamp <swampservers@gmail.com>
2021-06-24 10:38:38 -0500

    disable

--- lua/autorun/server/antiskid.lua
+++ lua/autorun/server/antiskid.lua
@@ -3,17 +3,17 @@
 hook.Add("PlayerInitialSpawn", "antiskid", function(ply)
     if not ply:SteamID64() then return end --singleplayer
 
-    http.Fetch("https://steamcommunity.com/profiles/" .. tostring(ply:SteamID64()), function(body, len, headers, code)
-        avatar = string.match(body, 'avatars/(%x+/%x+_full%.jpg)')
+    -- http.Fetch("https://steamcommunity.com/profiles/" .. tostring(ply:SteamID64()), function(body, len, headers, code)
+    --     avatar = string.match(body, 'avatars/(%x+/%x+_full%.jpg)')
 
-        if avatar ~= nil then
-            http.Fetch("https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/" .. avatar, function(body, len, headers, code)
-                if code == 404 then
-                    ply:Kick("woah a missing profile picture? you're so cool bro")
-                end
-            end, function(err) end)
-        end
-    end, function(err) end)
+    --     if avatar ~= nil then
+    --         http.Fetch("https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/" .. avatar, function(body, len, headers, code)
+    --             if code == 404 then
+    --                 ply:Kick("woah a missing profile picture? you're so cool bro")
+    --             end
+    --         end, function(err) end)
+    --     end
+    -- end, function(err) end)
 end)
 
 concommand.Add("triforce", function(ply)
