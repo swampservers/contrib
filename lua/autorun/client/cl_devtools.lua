905ee0ba4c5ee23743987c4de54b6d6fe268173f
Swamp <swampservers@gmail.com>
2021-06-24 21:24:10 -0500

    glualint

--- lua/autorun/client/cl_devtools.lua
+++ lua/autorun/client/cl_devtools.lua
@@ -1,6 +1,5 @@
 ﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
 -- INSTALL: CINEMA
-
 --[[
     Limitations:
         - If a path has lua/STRUCTHERE eg. "lua/autorun/lua/weapons/test.lua" then
@@ -8,34 +7,39 @@
         - A file within a sub directory of a struct folder like: "lua/weapons/testEnt/mysubdir/lua"
             Won't have the SWEP/struct table passed to it.
 
-]]--
+]]
+--
 SWAMP_DEV = SWAMP_DEV or {}
 
 SWAMP_DEV.allowed = {
     ["STEAM_0:0:105777797"] = true -- Nosharp https://github.com/nosharp
+    
 }
 
 -- Are we in development mode?
 SWAMP_DEV.enabled = false
 -- The delay on checking if we need to refresh files
 SWAMP_DEV.refreshDelay = 2
-
 local currentItemStruct = nil
-
 --- This stores file times for certain root folder.
 --- FileNames[key] -> SaveTime[value]
 SWAMP_DEV.fileTimes = SWAMP_DEV.fileTimes or {}
- 
--- lua/STRUCT/*
 
+-- lua/STRUCT/*
 SWAMP_DEV.structEnvironments = {
     ["weapons"] = function(curClass)
         local found = weapons.GetStored(curClass)
-        return found and {SWEP=found} or nil
+
+        return found and {
+            SWEP = found
+        } or nil
     end,
     ["entities"] = function(curClass)
         local found = scripted_ents.GetStored(curClass)
-        return found and {ENT=found} or nil
+
+        return found and {
+            ENT = found
+        } or nil
     end,
     ["effects"] = function(curClass)
         local allEffects = effects.GetList()
@@ -55,29 +59,33 @@ local ROOT_DIRECTORY = "addons/contrib/"
 local refreshFolder
 local loadFile
 local engineGamemode = engine.ActiveGamemode()
-do 
+
+do
     local loadFunc = function() end
+
     function loadFile(filePath, environmentVars)
-     
         local code = file.Read(filePath, "MOD")
-        if not code then 
+
+        if not code then
             error("No file exists with the file path: " .. filePath)
         end
+
         environmentVars = environmentVars or {}
         environmentVars["GM"] = engineGamemode
-        for key,var in pairs(environmentVars) do
+
+        for key, var in pairs(environmentVars) do
             _G[key] = var
         end
+
         xpcall(function()
             RunString(code, "SwampDevTools/" .. filePath)
-            
         end, function(err)
             print("[contrib] Error during loading file with devtools:", err)
             debug.Trace()
         end)
 
         -- remove the variaables from the global.
-        for key,_ in pairs(environmentVars) do
+        for key, _ in pairs(environmentVars) do
             _G[key] = nil
         end
     end
@@ -87,10 +95,10 @@ do
     function refreshFolder(subDir)
         local fileNames, folders = file.Find(subDir .. "/*", "MOD")
         local fileTimes = SWAMP_DEV.fileTimes
-        for _,fileName in ipairs(fileNames) do
+
+        for _, fileName in ipairs(fileNames) do
             local filePath = subDir .. "/" .. fileName
             if not fileName:EndsWith(".lua") then continue end
-
             local fTime = fileTime(filePath, "MOD")
 
             if not fileTimes[filePath] then
@@ -101,23 +109,25 @@ do
             end
 
             local wasFoundDuringLookup = false
+
             for structName, func in pairs(SWAMP_DEV.structEnvironments) do
                 local structLookupPath = "lua/" .. structName
 
                 if filePath:find(structLookupPath) then
-                    local className = fileName:sub(1,-5) -- remove the .lua extension.
+                    local className = fileName:sub(1, -5) -- remove the .lua extension.
                     loadFile(filePath, func(className) or func(subDir:gsub(".*/", "")))
                     wasFoundDuringLookup = true
                 end
             end
 
-            if not wasFoundDuringLookup then 
+            if not wasFoundDuringLookup then
                 loadFile(filePath)
             end
+
             fileTimes[filePath] = fTime
         end
 
-        for _,folder in ipairs(folders) do
+        for _, folder in ipairs(folders) do
             refreshFolder(subDir .. "/" .. folder)
         end
     end
@@ -126,7 +136,6 @@ end
 -- toggle dev editing. Root is addons/contrib
 concommand.Add("dev", function()
     if not SWAMP_DEV.allowed[LocalPlayer():SteamID()] then return end
-
     SWAMP_DEV.enabled = not SWAMP_DEV.enabled
 
     if SWAMP_DEV.enabled then
@@ -136,7 +145,6 @@ concommand.Add("dev", function()
             -- avoid checking unnecessary folders
             refreshFolder(ROOT_DIRECTORY .. "lua")
             refreshFolder(ROOT_DIRECTORY .. "gamemodes")
-
         end)
     else
         print("Dev mode disabled")
@@ -148,14 +156,13 @@ end, nil, "Toggle dev mode, editing in addons/contrib", FCVAR_UNREGISTERED)
 concommand.Add("dev_refresh", function(_, _, args)
     local filePath = args[1]
     if not SWAMP_DEV.enabled or not filePath then return end
-    print("Attempting to force refresh file " ..filePath)
-    
-    local structFound 
+    print("Attempting to force refresh file " .. filePath)
+    local structFound
     local ran
+
     for fileType in filePath:gmatch("[^/\\]+") do
-        
         if structFound then
-            loadFile(filePath, structFound(fileType)) 
+            loadFile(filePath, structFound(fileType))
             ran = true
             break
         end
@@ -163,7 +170,7 @@ concommand.Add("dev_refresh", function(_, _, args)
         structFound = SWAMP_DEV.structEnvironments[fileType]
     end
 
-    if not ran then 
+    if not ran then
         loadFile(filePath, {})
     end
 
