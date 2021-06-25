a2d63763cb985965d40fc1c37c252dbcac3a1195
Swamp <swampservers@gmail.com>
2021-06-24 22:29:56 -0500

    revert

--- lua/autorun/server/sv_monster.lua
+++ lua/autorun/server/sv_monster.lua
@@ -52,25 +52,23 @@ function ReleaseMonster(ply)
     end
 
     if ply.cantStartmonster then
-        --and not (ply.Obesity and ply:Obesity()>40) then
         if not ply.HVP_EVOLVED then
             ply.realFov = ply:GetFOV()
             ply.realWalkSpeed = ply:GetWalkSpeed()
             ply.realRunSpeed = ply:GetRunSpeed()
-            local ratio = ply.realRunSpeed / 300
-            ply:SetWalkSpeed(280 * ratio)
-            ply:SetRunSpeed(420 * ratio)
+            ply:SetWalkSpeed(280)
+            ply:SetRunSpeed(420)
             ply:SetFOV(ply.realFov + 10, 1)
-        end
 
-        timer.Simple(10, function()
-            if IsValid(ply) then
-                ply.cantStartmonster = false
-                ply:SetWalkSpeed(ply.realWalkSpeed)
-                ply:SetRunSpeed(ply.realRunSpeed)
-                ply:SetFOV(ply.realFov, 1)
-            end
-        end)
+            timer.Simple(10, function()
+                if IsValid(ply) then
+                    ply.cantStartmonster = false
+                    ply:SetWalkSpeed(ply.realWalkSpeed)
+                    ply:SetRunSpeed(ply.realRunSpeed)
+                    ply:SetFOV(ply.realFov, 1)
+                end
+            end)
+        end
     end
 
     ply.monsterCount = 0
