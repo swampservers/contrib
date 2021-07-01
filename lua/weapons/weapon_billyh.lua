e93525e63f96fc106d62b4e9f9c366add42f121c
PYROTEKNIK <laynedelowsky@gmail.com>
2021-06-30 19:56:18 -0700

    Update weapon_billyh.lua (#273)
    fixed so it doesn't give you pistol ammo for no reason

--- lua/weapons/weapon_billyh.lua
+++ lua/weapons/weapon_billyh.lua
@@ -5,6 +5,17 @@ SWEP.Slot = 2
 SWEP.ViewModel = ""
 SWEP.WorldModel = ""
 SWEP.HoldType = "normal"
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.Damage = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "none"
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Damage = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = "none"
+
 
 function SWEP:Initialize()
     self:SetHoldType("normal")
