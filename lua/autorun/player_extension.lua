545dc02ddd526d3827bf30d0aa91f6fdedbff3eb
PYROTEKNIK <laynedelowsky@gmail.com>
2021-07-17 21:02:42 -0700

    cringe little animation override that makes it take GetLaggedMovementValue into account. basically just the default hook but edited.

--- lua/autorun/player_extension.lua
+++ lua/autorun/player_extension.lua
@@ -62,6 +62,58 @@ function StripNameAdvert(name, advert)
     return n2
 end
 
+
+--gay little override for default animation
+timer.Simple(0,function()
+    local GM = gmod.GetGamemode()
+function GM:UpdateAnimation( ply, velocity, maxseqgroundspeed )
+
+	local len = velocity:Length()
+	local movement = 1.0
+
+	if ( len > 0.2 ) then
+		movement = ( len / maxseqgroundspeed )
+	end
+
+	local rate = math.min( movement, 2 )
+
+	-- if we're under water we want to constantly be swimming..
+	if ( ply:WaterLevel() >= 2 ) then
+		rate = math.max( rate, 0.5 )
+	elseif ( !ply:IsOnGround() && len >= 1000 ) then
+		rate = 0.1
+	end
+    rate = rate * ply:GetLaggedMovementValue()
+	ply:SetPlaybackRate( rate )
+
+	-- We only need to do this clientside..
+	if ( CLIENT ) then
+		if ( ply:InVehicle() ) then
+			--
+			-- This is used for the 'rollercoaster' arms
+			--
+			local Vehicle = ply:GetVehicle()
+			local Velocity = Vehicle:GetVelocity()
+			local fwd = Vehicle:GetUp()
+			local dp = fwd:Dot( Vector( 0, 0, 1 ) )
+
+			ply:SetPoseParameter( "vertical_velocity", ( dp < 0 && dp || 0 ) + fwd:Dot( Velocity ) * 0.005 )
+
+			-- Pass the vehicles steer param down to the player
+			local steer = Vehicle:GetPoseParameter( "vehicle_steer" )
+			steer = steer * 2 - 1 -- convert from 0..1 to -1..1
+			if ( Vehicle:GetClass() == "prop_vehicle_prisoner_pod" ) then steer = 0 ply:SetPoseParameter( "aim_yaw", math.NormalizeAngle( ply:GetAimVector():Angle().y - Vehicle:GetAngles().y - 90 ) ) end
+			ply:SetPoseParameter( "vehicle_steer", steer )
+
+		end
+		GAMEMODE:GrabEarAnimation( ply )
+		GAMEMODE:MouthMoveAnimation( ply )
+	end
+
+end
+end)
+
+
 -- local stripme = {"- swamp.sv", "-swamp.sv", "swamp.sv"}
 function PLAYER:ComputeName()
     if self:IsBot() then return "Kleiner" end
