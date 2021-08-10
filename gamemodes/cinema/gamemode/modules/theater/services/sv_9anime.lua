0f1e908bd55345a75a541526fe510c946598fa49
Noz <redmathred@gmail.com>
2021-08-09 21:35:34 -0400

    9Anime Support (#294)

---
+++ gamemodes/cinema/gamemode/modules/theater/services/sv_9anime.lua
@@ -0,0 +1,7 @@
+﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
+-- INSTALL: CINEMA
+sv_GetVideoInfo = sv_GetVideoInfo or {}
+
+sv_GetVideoInfo["9anime"] = function(self, key, ply, onSuccess, onFailure)
+    theater.GetVideoInfoClientside(self:GetClass(), key, ply, onSuccess, onFailure)
+end
