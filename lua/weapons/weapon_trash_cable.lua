de14c37de6832d1166b34d72e13b31abbacbf53d
Swamp <swampservers@gmail.com>
2021-09-08 15:37:24 -0500

    changes

--- lua/weapons/weapon_trash_cable.lua
+++ lua/weapons/weapon_trash_cable.lua
@@ -1,15 +1,15 @@
 ﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
 -- INSTALL: CINEMA
-SWEP.PrintName = "DuckTape"
+SWEP.PrintName = "Cable Maker"
 SWEP.DrawAmmo = false
 SWEP.ViewModelFOV = 85
 SWEP.Slot = 5
 SWEP.SlotPos = 0
 SWEP.Purpose = "Build things"
-SWEP.Instructions = "Primary: Tape Object Down\nSecondary: Untape object\nReload: Destroy item (or quick spawn trash)\n\nTip: Get another player to hold a prop in position while you tape it!"
+SWEP.Instructions = "Primary: Make cables (hold and release)"
 SWEP.AdminSpawnable = false
-SWEP.ViewModel = Model("models/swamponions/ducktape.mdl")
-SWEP.WorldModel = Model("models/swamponions/ducktape.mdl")
+SWEP.ViewModel		= "models/weapons/c_toolgun.mdl"
+SWEP.WorldModel		= "models/weapons/w_toolgun.mdl"
 SWEP.AutoSwitchTo = false
 SWEP.AutoSwitchFrom = false
 SWEP.Primary.ClipSize = -1
@@ -20,130 +20,102 @@ SWEP.Primary.Ammo = "none"
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Damage = -1
-SWEP.Secondary.Automatic = true
+SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
 
+CABLE_MAX_LENGTH = 100
+
 function SWEP:Initialize()
-    self:SetHoldType("slam")
+    self:SetHoldType("pistol")
 end
 
-function SWEP:DrawWorldModel()
-    local ply = self:GetOwner()
-    self:SetModelScale(0.8)
-
-    if (IsValid(ply)) then
-        local bn = ply:IsPony() and "LrigScull" or "ValveBiped.Bip01_R_Hand"
-        local bon = ply:LookupBone(bn) or 0
-        local opos = self:GetPos()
-        local oang = self:GetAngles()
-        local bp, ba = ply:GetBonePosition(bon)
-
-        if (bp) then
-            opos = bp
-        end
-
-        if (ba) then
-            oang = ba
-        end
-
-        if ply:IsPony() then
-            opos = opos + oang:Forward() * 9.5
-            opos = opos + oang:Up() * 3.6
-            opos = opos + oang:Right() * -3.7
-            oang:RotateAroundAxis(oang:Right(), 180)
-            oang:RotateAroundAxis(oang:Up(), -90)
-        else
-            opos = opos + oang:Right() * 8.6
-            opos = opos + oang:Forward() * 3.5
-            opos = opos + oang:Up() * 0.4
-            oang:RotateAroundAxis(oang:Right(), 180)
-            oang:RotateAroundAxis(oang:Forward(), -90)
-        end
-
-        self:SetupBones()
-        local mrt = self:GetBoneMatrix(0)
-
-        if (mrt) then
-            mrt:SetTranslation(opos)
-            mrt:SetAngles(oang)
-            self:SetBoneMatrix(0, mrt)
-        end
-    end
-
-    self:DrawModel()
-end
-
-function SWEP:GetViewModelPosition(pos, ang)
-    pos = pos + ang:Right() * 8
-    pos = pos + ang:Up() * -12
-    pos = pos + ang:Forward() * 18
-    ang:RotateAroundAxis(ang:Up(), 110)
-    ang:RotateAroundAxis(ang:Forward(), -60)
-
-    return pos, ang
-end
+local trashcabledata = nil
 
 function SWEP:PrimaryAttack()
     self:SetNextPrimaryFire(CurTime() + 0.3)
     if not IsFirstTimePredicted() then return end
 
     if CLIENT then
-        if PropTrashLookedAt then
-            net.Start("TrashAction")
-
-            net.WriteTable({
-                ent = PropTrashLookedAt,
-                act = TRASHACT_TAPE,
-                hitpos = PropTrashLookedAtPos
-            })
-
-            net.SendToServer()
+    if IsValid(PropTrashLookedAt) then
+            trashcabledata = {PropTrashLookedAt, PropTrashLookedAt:WorldToLocal(PropTrashLookedAtPos)}
         end
     end
+
+    -- if CLIENT then
+    --     if PropTrashLookedAt then
+    --         net.Start("TrashAction")
+
+    --         net.WriteTable({
+    --             ent = PropTrashLookedAt,
+    --             act = TRASHACT_TAPE,
+    --             hitpos = PropTrashLookedAtPos
+    --         })
+
+    --         net.SendToServer()
+    --     end
+    -- end
 end
+
+if CLIENT then
+    hook.Add("KeyRelease", "FinishTrashCable", function(ply,key)
+        if key == IN_ATTACK then
+            if trashcabledata and IsValid(trashcabledata[1]) then
+                if IsValid(PropTrashLookedAt) or WorldLookedAtPos then
+
+                    net.Start("TrashAction")
+
+                    net.WriteTable({
+                        ent = trashcabledata[1],
+                        entpos = trashcabledata[2],
+                        ent2 = PropTrashLookedAt or game.GetWorld(),
+                        ent2pos = WorldLookedAtPos or PropTrashLookedAt:WorldToLocal(PropTrashLookedAtPos),
+                        act = "cable"
+                    })
+
+                    net.SendToServer()
+
+                end
+            end
+            trashcabledata = nil
+        end
+    end)
+
+    local beem =  Material( "effects/tool_tracer" )
+
+    hook.Add("PostDrawTranslucentRenderables", "TrashCablePreview", function(d,s,s3)
+        if d or s3 then return end
+        if trashcabledata and IsValid(trashcabledata[1]) then
+            render.SetMaterial(beem)
+            local p1,p2 = trashcabledata[1]:LocalToWorld(trashcabledata[2]), PropTrashLookedAtPos or WorldLookedAtPos or (EyePos() + EyeAngles():Forward()*80)
+            local c = Color( 255, 255, 255 )
+
+            if trashcabledata[1]~=PropTrashLookedAt and (PropTrashLookedAtPos or WorldLookedAtPos) then
+                c = Color(0,255,0)
+            end
+            if p1:Distance(p2) > CABLE_MAX_LENGTH then
+                c = Color(255,55,0)
+            end
+            local r = 0 --math.random()
+            render.DrawBeam(p1,p2, 2, r - p1:Distance(p2)*0.1, p1:Distance(p2)*0.1 + 1 + r, c )
+        end
+    end)
+end
+
 
 function SWEP:SecondaryAttack()
-    self:SetNextSecondaryFire(CurTime() + 0.3)
-    if not IsFirstTimePredicted() then return end
+    net.Start("TrashAction")
 
-    if CLIENT then
-        if PropTrashLookedAt then
-            net.Start("TrashAction")
+    net.WriteTable({
+        ent = PropTrashLookedAt,
+        act = "uncable"
+    })
 
-            net.WriteTable({
-                ent = PropTrashLookedAt,
-                act = TRASHACT_UNTAPE,
-                hitpos = PropTrashLookedAtPos
-            })
-
-            net.SendToServer()
-        end
-    end
+    net.SendToServer()
 end
 
-local LastReloadTime = 0
 
 function SWEP:Reload()
-    if CLIENT then
-        if LastReloadTime + 0.3 > CurTime() then return end
-        LastReloadTime = CurTime()
 
-        if PropTrashLookedAt then
-            net.Start("TrashAction")
-
-            net.WriteTable({
-                ent = PropTrashLookedAt,
-                act = TRASHACT_REMOVE,
-                hitpos = PropTrashLookedAtPos
-            })
-
-            net.SendToServer()
-        else
-            SS_BuyProduct('trash')
-        end
-    end
-
-    return false
 end
 
 function SWEP:DrawHUD()
@@ -151,30 +123,3 @@ function SWEP:DrawHUD()
     surface.DrawCircle(ScrW() / 2, ScrH() / 2, 1, Color(255, 255, 255, 10))
 end
 
-if CLIENT then
-    --Global
-    PropTrashLookedAt = nil
-    PropTrashLookedAtPos = nil
-
-    hook.Add("Think", "TrashToolUpdate", function()
-        PropTrashLookedAt = nil
-
-        if IsValid(LocalPlayer()) and IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass():StartWith("weapon_trash") then
-            local self = LocalPlayer():GetActiveWeapon()
-            local tr = {}
-            tr.start = self.Owner:GetShootPos()
-            tr.endpos = self.Owner:GetShootPos() + (self.Owner:GetAimVector() * 120)
-            tr.filter = self.Owner
-            tr.mask = MASK_SHOT
-            local trace = util.TraceLine(tr)
-
-            if trace.Hit then
-                --:GetClass():StartWith("prop_trash") then
-                if trace.Entity:GetTrashClass() then
-                    PropTrashLookedAt = trace.Entity
-                    PropTrashLookedAtPos = trace.HitPos
-                end
-            end
-        end
-    end)
-end
