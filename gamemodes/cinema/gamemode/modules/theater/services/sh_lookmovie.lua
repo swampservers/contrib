16f59e7efd9deb487dc2bee8cab105046d3f5c96
NottyNoz <redmathred@gmail.com>
2021-10-18 09:46:44 -0400

    LookMovie Fix

--- gamemodes/cinema/gamemode/modules/theater/services/sh_lookmovie.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sh_lookmovie.lua
@@ -2,7 +2,7 @@
 -- INSTALL: CINEMA
 local SERVICE = {}
 SERVICE.Name = "LookMovie"
-SERVICE.Mature = true
+--SERVICE.Mature = true
 SERVICE.NeedsCodecs = true
 SERVICE.CacheLife = 0
 
@@ -14,8 +14,58 @@ function SERVICE:GetKey(url)
 end
 
 if CLIENT then
+    function SERVICE:GetVideoInfoClientside(key, callback)
+        local info = {}
+
+        local onFetchReceive = function(code, body, headers)
+            local t = util.JSONToTable(body)
+
+            theater.Services.base:Fetch(t["streams"]["1080p"], function(sbody)
+                local duration = 0
+                for k, v in ipairs(string.Split(sbody, "\n")) do
+                    if (v:StartWith("#EXTINF:")) then
+                        duration = duration + tonumber(string.Split(string.sub(v, 9), ",")[1])
+                    end
+                end
+                info.duration = math.ceil(duration)
+
+                info.data = t["streams"]["1080p"] --change to t["streams"] and load based on quality setting?
+                PrintTable(info)
+                callback(info)
+            end, callback)
+        end
+
+        HTTP({
+            method = "GET",
+            url = key,
+            headers = {["Cache-Control"] = "no-cache"},
+            success = function(code, body, headers)
+                local cookies = ""
+                for s,ss in string.gmatch(headers["Set-Cookie"],"([%w]+)=(.-);") do
+                    if (s == "PHPSESSID" or s == "csrf") then cookies = cookies..s.."="..ss.."; " end
+                end
+
+                info.title = string.match(body,"title: '(.-)'").." ("..string.match(body,"year: '(.-)'")..")"
+                info.thumb = string.match(body,"movie_poster: '(.-)'")
+
+                HTTP({
+                    method = "GET",
+                    url = "https://lookmovie.io/api/v1/security/movie-access?id_movie="..string.match(body,"id_movie: (%d+)"),
+                    headers = {
+                        ["Cache-Control"] = "no-cache",
+                        ["Cookie"] = cookies
+                    },
+                    success = onFetchReceive,
+                    failed = callback
+                })
+            end,
+            failed = callback
+        })
+    end
+
     function SERVICE:LoadVideo(Video, panel)
         local k = Video:Data()
+
         local url = "http://swamp.sv/s/cinema/hls.html"
         panel:EnsureURL(url)
 
