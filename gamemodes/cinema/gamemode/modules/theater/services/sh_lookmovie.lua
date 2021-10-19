12fda7026a0fa1034552a261915212e7970654aa
Noz <redmathred@gmail.com>
2021-10-19 14:11:22 -0400

    LookMovie Fix 2 (#304)
    * LookMovie Fix 2
    
    * hls player framework migration
    
    * .

--- gamemodes/cinema/gamemode/modules/theater/services/sh_lookmovie.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sh_lookmovie.lua
@@ -8,50 +8,67 @@ SERVICE.CacheLife = 0
 
 function SERVICE:GetKey(url)
     if (util.JSONToTable(url.encoded)) then return false end
-    if string.match(url.encoded, "lookmovie.io/movies/view/(.+)") and not string.find(url.path, "%.") then return url.encoded end
+    if string.match(url.encoded, "lookmovie.io/movies/view/(.+)") then return url.encoded end
 
     return false
 end
 
 if CLIENT then
+    lookmovieCookies = lookmovieCookies or ""
+
     function SERVICE:GetVideoInfoClientside(key, callback)
         local info = {}
 
         local onFetchReceive = function(code, body, headers)
             local t = util.JSONToTable(body)
+            if (t == nil) then callback()
+            else
+                local url = t["streams"]["1080p"] or t["streams"]["720p"] or t["streams"]["480p"] or t["streams"]["360p"]
+                local subs = ""
 
-            theater.Services.base:Fetch(t["streams"]["1080p"], function(sbody)
-                local duration = 0
-
-                for k, v in ipairs(string.Split(sbody, "\n")) do
-                    if (v:StartWith("#EXTINF:")) then
-                        duration = duration + tonumber(string.Split(string.sub(v, 9), ",")[1])
+                --find a good way to implement subtitle track choosing? lookmovie can have dozens of tracks and over a dozen tracks just for english with varying quality
+                --[[for k,v in pairs(t["subtitles"]) do
+                    if v["language"] == "English" then
+                        subs = "https://lookmovie.io"..v["file"]
+                        break
                     end
-                end
+                end]]
 
-                info.duration = math.ceil(duration)
-                info.data = t["streams"]["1080p"] --change to t["streams"] and load based on quality setting?
-                PrintTable(info)
-                callback(info)
-            end, callback)
+                info.data = util.TableToJSON({url=url,subs=subs}) --change to t["streams"] and load based on quality setting?
+
+                theater.Services.base:Fetch(url, function(sbody)
+                    local duration = 0
+
+                    for k, v in ipairs(string.Split(sbody, "\n")) do
+                        if (v:StartWith("#EXTINF:")) then
+                            duration = duration + tonumber(string.Split(string.sub(v, 9), ",")[1])
+                        end
+                    end
+
+                    info.duration = math.ceil(duration)
+                    if (LocalPlayer().videoDebug) then PrintTable(info) end
+                    callback(info)
+                end, callback)
+            end
         end
 
         HTTP({
             method = "GET",
             url = key,
             headers = {
-                ["Cache-Control"] = "no-cache"
+                ["Cache-Control"] = "no-cache",
+                ["Referer"] = key,
+                ["Cookie"] = lookmovieCookies
             },
             success = function(code, body, headers)
-                local cookies = ""
-
-                for s, ss in string.gmatch(headers["Set-Cookie"], "([%w]+)=(.-);") do
-                    if (s == "PHPSESSID" or s == "csrf") then
-                        cookies = cookies .. s .. "=" .. ss .. "; "
+                if headers["Set-Cookie"] then
+                    lookmovieCookies = ""
+                    for s,ss in string.gmatch(headers["Set-Cookie"],"([%w]+)=(.-);") do
+                        if (s == "PHPSESSID" or s == "csrf") then lookmovieCookies = lookmovieCookies..s.."="..ss.."; " end
                     end
                 end
 
-                info.title = string.match(body, "title: '(.-)'") .. " (" .. string.match(body, "year: '(.-)'") .. ")"
+                info.title = string.Replace(string.match(body, "title: '(.-)',\n"), "\\'", "'") .. " (" .. string.match(body, "year: '(.-)'") .. ")"
                 info.thumb = string.match(body, "movie_poster: '(.-)'")
 
                 HTTP({
@@ -59,7 +76,8 @@ if CLIENT then
                     url = "https://lookmovie.io/api/v1/security/movie-access?id_movie=" .. string.match(body, "id_movie: (%d+)"),
                     headers = {
                         ["Cache-Control"] = "no-cache",
-                        ["Cookie"] = cookies
+                        ["Referer"] = key,
+                        ["Cookie"] = lookmovieCookies
                     },
                     success = onFetchReceive,
                     failed = callback
@@ -70,17 +88,14 @@ if CLIENT then
     end
 
     function SERVICE:LoadVideo(Video, panel)
-        local k = Video:Data()
-        local url = "http://swamp.sv/s/cinema/hls.html"
+        local t = util.JSONToTable(Video:Data())
+        local url = "http://swamp.sv/s/cinema/file.html"
         panel:EnsureURL(url)
 
-        --using a 2 second delay is the fastest way to load the video, sending th_video any quicker is much much slower for whatever reason
-        timer.Simple(2, function()
-            if IsValid(panel) then
-                local str = string.format("th_video('%s',%s);", string.JavascriptSafe(k), true)
-                panel:QueueJavascript(str)
-            end
-        end)
+        if IsValid(panel) then
+            local str = string.format("th_video('%s','%s');", string.JavascriptSafe(t.url), t.subs or "")
+            panel:QueueJavascript(str)
+        end
     end
 end
 
