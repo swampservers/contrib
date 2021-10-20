08bd1893c8d8d114d57fa8363744376fa39173f9
NottyNoz <redmathred@gmail.com>
2021-10-20 00:53:09 -0400

    neko update

--- gamemodes/cinema/gamemode/modules/theater/services/sh_neko.lua
+++ gamemodes/cinema/gamemode/modules/theater/services/sh_neko.lua
@@ -23,64 +23,19 @@ if CLIENT then
     end
 
     function SERVICE:LoadVideo(Video, panel)
-        panel:EnsureURL(Video:Key())
-        panel:QueueJavascript("document.getElementsByClassName('header-container')[0].style.display='none';document.getElementsByClassName('room-container')[0].style.display='none';document.getElementsByClassName('video-menu')[0].style.display='none';document.getElementsByClassName('neko-menu')[0].style.display='none';document.getElementsByClassName('connect')[0].style.display='none';document.getElementsByClassName('vue-notification-group')[0].style.display='none';") --hide ui
-        panel.phase = 0
+        panel:EnsureURL(Video:Key() .. "/?pwd=tgclub&usr=" .. LocalPlayer():SteamID() .. "&cast=1")
 
         timer.Create("nekoupdate" .. tostring(math.random(1, 100000)), 1, 45, function()
             if IsValid(panel) then
-                panel:RunJavascript("document.getElementsByClassName('vue-notification-group')[0].style.display='none';document.getElementsByClassName('header-container')[0].style.display='none';document.getElementsByClassName('room-container')[0].style.display='none';document.getElementsByClassName('video-menu')[0].style.display='none';document.getElementsByClassName('neko-menu')[0].style.display='none';document.getElementsByClassName('connect')[0].style.display='none';") --failsafe hide ui
-
-                --panel:RunJavascript("function th_volume(vol){document.getElementsByTagName('input')[1].value=vol;document.getElementsByTagName('input')[1].dispatchEvent(new Event('input'));}") --volume control
-                if panel.phase == 0 then
-                    panel:RunJavascript("var nekoparent=document.getElementById('neko');console.log(nekoparent.getElementsByTagName('input').length);console.log('LOGIN:'+document.getElementsByClassName('connect').length);") --check if already logged in
-                    panel:RunJavascript("if(document.getElementsByClassName('neko-menu').length==0){document.getElementsByTagName('i')[1].click();}document.getElementsByClassName('tabs-container')[0].getElementsByTagName('i')[1].click();") --switch to settings tab
-                    panel:RunJavascript("if(document.getElementsByTagName('input')[6].checked){document.getElementsByTagName('input')[6].click();}else{document.getElementsByClassName('neko-menu')[0].remove();}") --disable chat sounds
-
-                    return
-                elseif panel.phase == 1 then
-                    panel:RunJavascript("nekoparent=document.getElementById('neko');var inputevent=new Event('input');nekoparent.getElementsByTagName('input')[2].value='" .. LocalPlayer():SteamID() .. "';nekoparent.getElementsByTagName('input')[3].value='tgclub';nekoparent.getElementsByTagName('input')[2].dispatchEvent(inputevent);nekoparent.getElementsByTagName('input')[3].value='tgclub';nekoparent.getElementsByTagName('input')[3].dispatchEvent(inputevent);nekoparent.getElementsByTagName('button')[0].click();") --log in
-                    panel:RunJavascript("console.log('LOGIN:'+document.getElementsByClassName('connect').length);")
-
-                    return
-                elseif panel.phase == 2 then
-                    panel:RunJavascript("if(document.getElementsByClassName('fa-volume-mute fas').length>0)document.getElementsByClassName('fa-volume-mute fas')[0].click();") --unmute player
-
-                    return
-                end
+                panel:RunJavascript("document.getElementsByTagName('p')[0].style.display='none';") --hides watermark
+                panel:RunJavascript("document.getElementsByTagName('video')[0].click();") --unmutes player
+                self:SetVolume(GetConVar("cinema_volume"):GetInt(), panel)
             end
         end)
-
-        function panel:ConsoleMessage(msg)
-            if msg then
-                local smsg = tostring(msg)
-
-                --required for debug in this case
-                if (LocalPlayer().videoDebug and not smsg:StartWith("HREF:")) then
-                    print(smsg)
-                end
-
-                if self.phase == 0 then
-                    if type(msg) == "number" and msg >= 4 then
-                        self.phase = 1
-                    elseif string.StartWith(smsg, "LOGIN:") and smsg:sub(7, -1) == "0" then
-                        self.phase = 2
-                    end
-
-                    return
-                elseif self.phase == 1 then
-                    if string.StartWith(smsg, "LOGIN:") and smsg:sub(7, -1) == "0" then
-                        self.phase = 2
-                    end
-
-                    return
-                end
-            end
-        end
     end
 
     function SERVICE:SetVolume(vol, panel)
-        local str = string.format("document.getElementsByTagName('input')[1].value=%s;document.getElementsByTagName('input')[1].dispatchEvent(new Event('input'));", vol)
+        local str = string.format("document.getElementsByTagName('video')[0].volume = %s", vol/100)
         panel:RunJavascript(str) --QueueJavascript is unreliable
     end
 end
