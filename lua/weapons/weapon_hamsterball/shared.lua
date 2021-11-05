fb30124d834260ea67dc06a41f865a66f312b8d1
Swamp <swampservers@gmail.com>
2021-11-04 21:01:10 -0500

    changes

--- lua/weapons/weapon_hamsterball/shared.lua
+++ lua/weapons/weapon_hamsterball/shared.lua
@@ -5,6 +5,13 @@ SWEP.Slot = 4
 SWEP.ViewModel = Model("models/dynamite/dynamite.mdl")
 SWEP.WorldModel = Model("models/dynamite/dynamite.mdl")
 
+print("TH")
+
+-- models/props_phx/ball.mdl
+-- models/props_phx/cannonball.mdl
+-- models/props_phx/cannonball_solid.mdl
+-- models/shadertest/shader3
+
 function SWEP:PrimaryAttack()
 
 end
@@ -15,4 +22,12 @@ end
 
 function SWEP:Deploy()
     self:SendWeaponAnim(ACT_SLAM_THROW_DRAW)
+end
+
+
+
+function SWEP:SetupMove(ply, mv, cmd)
+    local vel = mv:GetVelocity()
+    vel.z = power
+    mv:SetVelocity(Vector(0,0,0))
 end
