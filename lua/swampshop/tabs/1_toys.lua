882fefb350cdc43835606d25af4a7ad339cf087e
Swamp <swampservers@gmail.com>
2021-11-08 12:13:19 -0600

    changes

--- lua/swampshop/tabs/1_toys.lua
+++ lua/swampshop/tabs/1_toys.lua
@@ -220,7 +220,7 @@ SS_WeaponProduct({
 SS_WeaponProduct({
     name = 'Police Taser',
     description = "An electroshock weapon capable of paralyzing other players for up to 20 seconds.",
-    price = 4000,
+    price = 400000,
     model = 'models/weapons/cg_ocrp2/w_taser.mdl',
     class = 'weapon_taser'
 })
