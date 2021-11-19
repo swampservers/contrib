5964e3c3bec27d20fcab6bebe2224183d51c9420
Swamp <swampservers@gmail.com>
2021-11-18 18:32:35 -0600

    changes

--- lua/swampshop/tabs/4_playermodels.lua
+++ lua/swampshop/tabs/4_playermodels.lua
@@ -332,6 +332,8 @@ SS_BoneModItem({
         end)
     end,
     ApplyBoneMod = function(self, ent)
+        -- {[self.cfg[ent:PMCS"bone"]]=true} -- 
+
         local bones = self.cfg[ent:PMCS"bones"] or {
             ["LrigScull"] = true,
             ["ValveBiped.Bip01_Head1"] = true
@@ -483,7 +485,8 @@ SS_PlayermodelItem({
 
         return "Use any playermodel from workshop! Once the model is finalized, it can't be changed."
     end,
-    GetModel = function(self) --     --     -- so the callback when downloaded makes the model refresh
+    GetModel = function(self) --     -- if self.specs.wsid or self.cfg.wsid then
+--     --     -- so the callback when downloaded makes the model refresh
 --     --     register_workshop_model(self.specs.model or self.cfg.model, self.specs.wsid or self.cfg.wsid)
 --     --     -- makes sure we download this addon when the item is viewed in shop, see autorun/sh_workshop.lua
 --     --     -- if self.Owner == LocalPlayer() then
@@ -492,7 +495,7 @@ SS_PlayermodelItem({
 --     -- end
 --     return 
 -- end
-return self.specs and (self.specs.model or self.cfg.model) or "models/maxofs2d/logo_gmod_b.mdl" -- if CLIENT and self.specs and (self.specs.model or (self.cfg.model and self.cfg.wsid)) then end, --     -- if self.specs.wsid or self.cfg.wsid then
+return self.specs and (self.specs.model or self.cfg.model) or "models/maxofs2d/logo_gmod_b.mdl" end, -- if CLIENT and self.specs and (self.specs.model or (self.cfg.model and self.cfg.wsid)) then
     GetWorkshop = function(self) return self.specs and (self.specs.wsid or self.cfg.wsid) end,
     invcategory = "Playermodels",
     playermodel = true,
