92a87e5132f827b8fa505c3826dc6267f9bac749
Swamp <swampservers@gmail.com>
2022-01-01 17:24:51 -0600

    notices

--- gamemodes/cinema/gamemode/init.lua
+++ gamemodes/cinema/gamemode/init.lua
@@ -1,3 +1,5 @@
 ﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
 AddCSLuaFile("cl_init.lua")
 include("cl_init.lua")
+
+-- THE GAMEMODE CODE IS NOW IN lua/cinema
