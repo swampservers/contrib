c7d6f8ccb4e1c77076b5ca0742c1c8e826cc60c6
Swamp <swampservers@gmail.com>
2022-02-14 21:58:41 -0600

    glualint

--- lua/swamp/misc/sh_titles.lua
+++ lua/swamp/misc/sh_titles.lua
@@ -4,6 +4,7 @@ local Player = FindMetaTable("Player")
 --- Get current title string or ""
 function Player:GetTitle()
     local title = self:GetNWString("title", "")
+
     return TitleDisplayFn[title] and TitleDisplayFn[title](self) or title
 end
 
@@ -11,7 +12,6 @@ end
 Titles = {}
 TitleRefreshDir = defaultdict(function() return {} end)
 RewardIdTitleDir = defaultdict(function() return {} end)
-
 TitleDisplayFn = {}
 
 -- not implemented lol
@@ -152,13 +152,6 @@ end
 --progress_fn: optional function to compute progess, defaults to summing nwp vars
 --group_view: optional, pset id and verb to track with
 -- TODO: make income max at 500k due to new ways to get points, make leaderboards network the threshold to get to a certain level
-
-
-
-
-
-
-
 AddTitle({
     {1, "Lover", 25000},
 }, "On Valentine's Day, accept a valentine from another player, or give one to someone and have them accept it", "s_valentine")
@@ -172,19 +165,15 @@ AddTitle({
 }, "Reject a valentine from %s different players", "s_rejected_size")
 
 AddTitle("valentine", "Show who your valentine is!", "valentine_t", {
-    progress_fn = function(ply) 
-        return ply.NWP.valentine_t~=nil 
-    end,
+    progress_fn = function(ply) return ply.NWP.valentine_t ~= nil end,
 })
 
-TitleDisplayFn.valentine = function(ply) return "❤ "..(ply.NW.ValentineName or "(name)") end
-    
+TitleDisplayFn.valentine = function(ply) return "❤ " .. (ply.NW.ValentineName or "(name)") end
 
 --jolly
 AddTitle({
     {1, "Festive", 25000},
 }, "During December, give a present (from shop) to another player", "s_christmas")
-
 
 -- {1000, "Saint Nick", 1000000}
 AddTitle({
