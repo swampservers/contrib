c3930bafe26bc3a99a3a99f1d8f03222236f2d8f
Swamp <swampservers@gmail.com>
2022-02-15 22:44:44 -0600

    glualint

--- lua/swamp/chat/sv_commands/ponyspeak.lua
+++ lua/swamp/chat/sv_commands/ponyspeak.lua
@@ -78,19 +78,17 @@ end, {
     throttle = true
 })
 
-
-
-if gm=="cinema" then
+if gm == "cinema" then
     RegisterChatCommand({'ponyrp'}, function(ply, arg)
         if IsValid(ply) and not ply:InVehicle() and ply:Alive() then
             ply:SetPos(Vector(-388, 1400, -118)) --treatment room location
-    
+
             for k, v in ipairs(ents.GetAll()) do
                 if IsValid(v) then
                     if v:GetName() == "treatmentdoor" then
                         v:Fire("Close")
                     end
-    
+
                     if v:GetName() == "treatmentlever" then
                         v:Fire("PressOut")
                     end
@@ -101,9 +99,8 @@ if gm=="cinema" then
         global = false,
         throttle = true
     })
-    
+
     hook.Add("PlayerSay", "TreatmentRoomChat", function(ply, text, team)
         if ply:GetLocationName() == "Treatment Room" and text ~= "/tpa" and text ~= "!tpa" then return "i like ponies" end
     end)
-    
-    end
+end
