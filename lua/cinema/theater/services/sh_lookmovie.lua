d5cdc2166ad4ef4f0fb405fc4c205e85051629c4
NottyNoz <redmathred@gmail.com>
2022-03-12 19:54:09 -0500

    LookMovie Fix 2

--- lua/cinema/theater/services/sh_lookmovie.lua
+++ lua/cinema/theater/services/sh_lookmovie.lua
@@ -6,16 +6,16 @@ SERVICE.NeedsCodecs = true
 SERVICE.CacheLife = 0
 
 --local domains = {"lmplayer.xyz", "contentmatserishere.com", "thisistheplacetowatch.com", "watchthesestuff.com", "bestofworldcontent.com", "wehaveallcontent.com", "bestalltimemovies.xyz", "contentforall.xyz", "watchmorestuff.xyz", "lookmovie%d+.xyz"}
-local domains = {"lookmovie.site", "lookmovie%d+.la"}
+--local domains = {"lookmovie.site", "lookmovie%d+.la"}
 
 function SERVICE:GetKey(url)
     if util.JSONToTable(url.encoded) then return false end
 
     --if string.match(url.encoded, "lookmovie.io/movies/view/(.+)") or string.match(url.encoded, "lookmovie.io/shows/view/(.+)#.+%-(%d+)$") then return url.encoded end
-    for _, v in pairs(domains) do
-        --if string.match(url.encoded, v .. "/m/./(.+)/s") or string.match(url.encoded, v .. "/s/./(.+)/s#") then return url.encoded end
-        if string.match(url.encoded, v .. "/%w+/play/(.+)") then return url.encoded end
-    end
+    --for _, v in pairs(domains) do
+    --    if string.match(url.encoded, v .. "/m/./(.+)/s") or string.match(url.encoded, v .. "/s/./(.+)/s#") then return url.encoded end
+    --end
+    if string.match(url.encoded, "lookmovie%d*%.%w+/%w+/play/(.+)") then return url.encoded end
 
     return false
 end
