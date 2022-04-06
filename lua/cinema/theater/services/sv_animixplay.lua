0b939680007452cf9ad1bf602e9cd94b314cf72e
NottyNoz <redmathred@gmail.com>
2022-04-05 23:31:05 -0400

    AniMixPlay Support

---
+++ lua/cinema/theater/services/sv_animixplay.lua
@@ -0,0 +1,6 @@
+﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
+sv_GetVideoInfo = sv_GetVideoInfo or {}
+
+sv_GetVideoInfo.animixplay = function(self, key, ply, onSuccess, onFailure)
+	theater.GetVideoInfoClientside(self:GetClass(), key, ply, onSuccess, onFailure)
+end
