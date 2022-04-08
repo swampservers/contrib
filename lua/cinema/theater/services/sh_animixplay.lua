e359d8d35c4db88cca5c240ee2da6e4558e41880
Swamp <swampservers@gmail.com>
2022-04-07 21:35:25 -0500

    glualint

--- lua/cinema/theater/services/sh_animixplay.lua
+++ lua/cinema/theater/services/sh_animixplay.lua
@@ -1,4 +1,4 @@
--- This file is subject to copyright - contact swampservers@gmail.com for more information.
+﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
 local SERVICE = {}
 SERVICE.Name = "AniMixPlay"
 SERVICE.NeedsCodecs = true
@@ -12,60 +12,64 @@ end
 
 if CLIENT then
     function SERVICE:GetVideoInfoClientside(key, callback)
-		http.Fetch(key, function(body)
-			local ep = string.match(key, "animixplay.to/v11?/.+/ep(%d+)") or 1
-			
-			EmbeddedCheckCodecs(function() --required because the http functions fail to get the location response header
-				if vpanel then
-					vpanel:Remove()
-				end
-				
-				vpanel = vgui.Create("TheaterHTML")
-				vpanel:SetSize(0,0)
-				vpanel:SetAlpha(0)
-				vpanel:SetMouseInputEnabled(false)
-				local duration
-				
-				timer.Simple(10, function()
-					if IsValid(vpanel) then
-						vpanel:Remove()
-						print("Failed")
-						callback()
-					end
-				end)
-				
-				timer.Create("animixplayupdate" .. tostring(math.random(1, 100000)), 1, 10, function()
-					if IsValid(vpanel) then
-						vpanel:RunJavascript("console.log('DURATION:'+Math.ceil(player1.duration))")
-					end
-				end)
-				
-				function vpanel:ConsoleMessage(msg)
-					if msg:StartWith("DURATION:") and not msg:StartWith("DURATION:0") then
-						duration = tonumber(string.sub(msg, 10))
-					elseif msg:StartWith("HREF:") and duration ~= nil then
-						local url = msg:sub(6)
-						if url ~= "about:blank" then
-							vpanel:Remove()
-							
-							callback({
-								title = string.match(body,"<span class=\"animetitle\">(.-)</span>") .. (string.match(body, "\"eptotal\":1,") and "" or (" - Episode " .. ep)),
-								data = string.Explode("#",url)[2],
-								duration = duration
-								--data = util.Base64Decode(string.Explode("#",url)[2])
-							})
-						end
-					end
-				end
-				
-				local k = string.match(body,"\""..(ep-1).."\":\".-php%?id=(.-)&title")
-				if (not k) then
-					chat.AddText(string.match(body, "\"eptotal\":0") and "[red]There are no episodes of this yet." or "[red]Use a different stream server.")
-					callback()
-				end
-				vpanel:OpenURL("https://animixplay.to/api/live"..util.Base64Encode(k.."LTXs3GrU8we9O"..util.Base64Encode(k))) --redirects to player page
-			end)
-		end)
+        http.Fetch(key, function(body)
+            local ep = string.match(key, "animixplay.to/v11?/.+/ep(%d+)") or 1
+
+            --required because the http functions fail to get the location response header
+            EmbeddedCheckCodecs(function()
+                if vpanel then
+                    vpanel:Remove()
+                end
+
+                vpanel = vgui.Create("TheaterHTML")
+                vpanel:SetSize(0, 0)
+                vpanel:SetAlpha(0)
+                vpanel:SetMouseInputEnabled(false)
+                local duration
+
+                timer.Simple(10, function()
+                    if IsValid(vpanel) then
+                        vpanel:Remove()
+                        print("Failed")
+                        callback()
+                    end
+                end)
+
+                timer.Create("animixplayupdate" .. tostring(math.random(1, 100000)), 1, 10, function()
+                    if IsValid(vpanel) then
+                        vpanel:RunJavascript("console.log('DURATION:'+Math.ceil(player1.duration))")
+                    end
+                end)
+
+                function vpanel:ConsoleMessage(msg)
+                    if msg:StartWith("DURATION:") and not msg:StartWith("DURATION:0") then
+                        duration = tonumber(string.sub(msg, 10))
+                    elseif msg:StartWith("HREF:") and duration ~= nil then
+                        local url = msg:sub(6)
+
+                        if url ~= "about:blank" then
+                            vpanel:Remove()
+
+                            callback({
+                                title = string.match(body, "<span class=\"animetitle\">(.-)</span>") .. (string.match(body, "\"eptotal\":1,") and "" or " - Episode " .. ep),
+                                data = string.Explode("#", url)[2],
+                                duration = duration
+                            })
+                            --data = util.Base64Decode(string.Explode("#",url)[2])
+                        end
+                    end
+                end
+
+                local k = string.match(body, "\"" .. ep - 1 .. "\":\".-php%?id=(.-)&title")
+
+                if not k then
+                    chat.AddText(string.match(body, "\"eptotal\":0") and "[red]There are no episodes of this yet." or "[red]Use a different stream server.")
+                    callback()
+                end
+
+                vpanel:OpenURL("https://animixplay.to/api/live" .. util.Base64Encode(k .. "LTXs3GrU8we9O" .. util.Base64Encode(k))) --redirects to player page
+            end)
+        end)
     end
 
     function SERVICE:LoadVideo(Video, panel)
@@ -78,7 +82,7 @@ if CLIENT then
             panel:QueueJavascript(string.format("target_time=%s;to_volume=%s;", Me:GetTheater():VideoCurrentTime(true), theater.GetVolume()))
         end
     end
-	
+
     function SERVICE:SetVolume(vol, panel)
         panel:RunJavascript(string.format("to_volume=%s;", vol))
     end
--- lua/cinema/theater/services/sv_animixplay.lua
+++ lua/cinema/theater/services/sv_animixplay.lua
@@ -2,5 +2,5 @@
 sv_GetVideoInfo = sv_GetVideoInfo or {}
 
 sv_GetVideoInfo.animixplay = function(self, key, ply, onSuccess, onFailure)
-	theater.GetVideoInfoClientside(self:GetClass(), key, ply, onSuccess, onFailure)
+    theater.GetVideoInfoClientside(self:GetClass(), key, ply, onSuccess, onFailure)
 end
