f204e4052e57fae02bf09b6e750486c83ecaafb4
Metroid55 <32679840+Metroid55@users.noreply.github.com>
2022-04-22 02:16:21 +0200

    More shotgun adjustments (#355)
    * slight spas12 nerf
    
    * shotgun damage
    
    * shotgun damage
    
    * shotgun damage
    
    * Slug range nerf
    
    * Spas12 firing speed
    
    The 10 Pellets per shot make this shotgun too op with that firing rate, therefore adjusting it further
    
    * Adjustment
    
    Actually don't want to overnerf slug with the dmg nerf from earlier

--- lua/cinema/guns/weapons/gun/shared.lua
+++ lua/cinema/guns/weapons/gun/shared.lua
@@ -159,7 +159,7 @@ function GunPerkOverrides(swep, perk)
         t.Damage = swep.Damage * swep.NumPellets
         t.NumPellets = 1
         t.PelletSpread = 0
-        t.HalfDamageDistance = swep.HalfDamageDistance * 2
+        t.HalfDamageDistance = swep.HalfDamageDistance * 1.7
         t.SpawnPriceMod = (swep.SpawnPriceMod or 1) * 1.5
         t.AmmoPriceMod = (swep.AmmoPriceMod or 1) * 1.8
     elseif perk == "selfloading" then
