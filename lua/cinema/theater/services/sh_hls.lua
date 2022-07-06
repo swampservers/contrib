324a190b12fd405762c5e9e4ff05a80582b1dd7c
Noz <redmathred@gmail.com>
2022-07-06 15:01:35 -0400

    Service Updates (#364)
    * Service Updates
    
    * hls
    
    * move

--- lua/cinema/theater/services/sh_hls.lua
+++ lua/cinema/theater/services/sh_hls.lua
@@ -8,77 +8,20 @@ SERVICE.CacheLife = 0
 
 function SERVICE:GetKey(url)
     if util.JSONToTable(url.encoded) then return false end
-    if string.sub(url.path, -5) == ".m3u8" or string.find(url.encoded, "streamwat.ch/(.+)") and not string.find(url.path, "%.") then return url.encoded end
+    if string.sub(url.path, -5) == ".m3u8" then return url.encoded end
 
     return false
 end
 
 if CLIENT then
     function SERVICE:GetVideoInfoClientside(key, callback)
-        if string.EndsWith(key, ".m3u8") then
-            Derma_StringRequest("HLS Stream Title", "Name your livestream:", Me:Nick() .. "'s Stream", function(title)
-                callback({
-                    title = title
-                })
-            end, function()
-                callback()
-            end)
-        else
-            EmbeddedCheckCodecs(function()
-                vpanel = vgui.Create("DHTML")
-                vpanel:SetSize(100, 100)
-                vpanel:SetAlpha(0)
-                vpanel:SetMouseInputEnabled(false)
-                local link = nil
-
-                timer.Simple(20, function()
-                    if IsValid(vpanel) then
-                        vpanel:Remove()
-                        print("Failed")
-                        callback()
-                    end
-                end)
-
-                function vpanel:ConsoleMessage(msg)
-                    if Me.videoDebug then
-                        print(msg)
-                    end
-
-                    if msg:StartWith("STREAMWATCHURL:") then
-                        vpanel:OpenURL("http://swamp.sv/s/cinema/hls.html")
-                        link = string.sub(msg, 16)
-                        local str = string.format("th_video('%s');", string.JavascriptSafe(link))
-
-                        --delayed so page can load
-                        timer.Simple(1, function()
-                            vpanel:QueueJavascript(str)
-                        end)
-                    end
-
-                    if msg:StartWith("LIVE") then
-                        self:Remove()
-                        print("Success!")
-
-                        Derma_StringRequest("HLS Stream Title", "Name your livestream:", Me:Nick() .. "'s Stream", function(title)
-                            callback({
-                                data = link,
-                                title = title
-                            })
-                        end, function()
-                            callback()
-                        end)
-                    end
-                end
-
-                vpanel:OpenURL(key)
-                local str = string.format("console.log('STREAMWATCHURL:'+document.title);", string.JavascriptSafe(key))
-                vpanel:QueueJavascript(str)
-            end, function()
-                chat.AddText("You need codecs to request this. Press F2.")
-
-                return callback()
-            end)
-        end
+        Derma_StringRequest("HLS Stream Title", "Name your livestream:", Me:Nick() .. "'s Stream", function(title)
+            callback({
+                title = title
+            })
+        end, function()
+            callback()
+        end)
     end
 
     function SERVICE:GetHost(Video)
--- lua/cinema/theater/services/sv_hls.lua
+++ lua/cinema/theater/services/sv_hls.lua
@@ -2,23 +2,7 @@
 sv_GetVideoInfo = sv_GetVideoInfo or {}
 
 sv_GetVideoInfo.hls = function(self, key, ply, onSuccess, onFailure)
-    local streamwatch_key = string.match(key, "streamwat.ch/(%w+)/*$")
-    local datalink = nil
-
-    local onReceive = function(info)
-        http.Post("https://swamp.sv/fedorabot/gis.php", {
-            q = info.title
-        }, function(body)
-            info.thumb = "http://swamp.sv/s/cinema/contain.php?i=" .. body
-            onSuccess(info)
-        end, onFailure)
-    end
-
     local onFetchReceive = function(body, length, headers, code)
-        if headers["Access-Control-Allow-Origin"] and headers["Access-Control-Allow-Origin"] ~= "*" and headers["Access-Control-Allow-Origin"] ~= "https://swamp.sv/" then
-            datalink = "https://cors.oak.re/" .. (datalink or key)
-        end
-
         local info = {}
         local duration = 0
         local timed = false
@@ -37,9 +21,9 @@ sv_GetVideoInfo.hls = function(self, key, ply, onSuccess, onFailure)
             ply:PrintMessage(HUD_PRINTCONSOLE, "#EXTM3U") --debug
 
             --use player to get the title
-            theater.GetVideoInfoClientside(self:GetClass(), datalink or key, ply, function(info)
+            theater.GetVideoInfoClientside(self:GetClass(), key, ply, function(info)
                 info.duration = 0
-                info.data = datalink or ""
+                info.data = ""
 
                 if timed then
                     info.duration = math.ceil(duration)
@@ -54,61 +38,32 @@ sv_GetVideoInfo.hls = function(self, key, ply, onSuccess, onFailure)
         end
     end
 
-    local onFetchReceiveStreamWatch = function(body, length, headers, code)
-        local streamwatch_url = string.match(body, "(http.+%.m3u8)")
+    --process the first link if it's a playlist/menu
+    self:Fetch(key, function(body)
+        local newurl = string.Split(key, "/")
+        local urlindex = nil
 
-        if streamwatch_url == nil or code == 0 then
-            --use player to get the hls link due to serverside http issue
-            theater.GetVideoInfoClientside(self:GetClass(), key, ply, function(info)
-                info.data = streamwatch_url
-                info.duration = 0
-                onReceive(info)
-            end, onFailure)
-        elseif streamwatch_url ~= nil then
-            datalink = string.Replace(streamwatch_url, "https://cors.oak.re/", "")
-            self:Fetch(datalink, onFetchReceive, onFailure)
-        else
-            ply:PrintMessage(HUD_PRINTCONSOLE, body) --debug
-            onFailure('Theater_RequestFailed')
+        for k, v in ipairs(string.Split(body, "\n")) do
+            if string.find(v, ".m3u8") and not urlindex then
+                urlindex = v
+            end
         end
-    end
 
-    if streamwatch_key ~= nil then
-        self:Fetch("http://streamwat.ch/" .. streamwatch_key .. "/player.min.js", onFetchReceiveStreamWatch, function()
-            --use player to get the hls link due to serverside http issue
-            theater.GetVideoInfoClientside(self:GetClass(), key, ply, function(info)
-                info.duration = 0
-                onSuccess(info)
-            end, onFailure)
-        end)
-    else
-        --process the first link if it's a playlist/menu
-        self:Fetch(key, function(body)
-            local newurl = string.Split(key, "/")
-            local urlindex = nil
+        if urlindex and not string.find(urlindex, "http.://") then
+            local backcount = #string.Split(urlindex, "..") - 1
 
-            for k, v in ipairs(string.Split(body, "\n")) do
-                if string.find(v, ".m3u8") and not urlindex then
-                    urlindex = v
-                end
+            for _ = 1, backcount do
+                table.remove(newurl)
             end
 
-            if urlindex and not string.find(urlindex, "http.://") then
-                local backcount = #string.Split(urlindex, "..") - 1
+            newurl[#newurl] = string.sub(urlindex, backcount * 3 + 1) or newurl[#newurl]
+            newurl = table.concat(newurl, "/")
+        elseif urlindex and string.find(urlindex, "http.://") then
+            newurl = urlindex
+        else
+            newurl = key
+        end
 
-                for _ = 1, backcount do
-                    table.remove(newurl)
-                end
-
-                newurl[#newurl] = string.sub(urlindex, backcount * 3 + 1) or newurl[#newurl]
-                newurl = table.concat(newurl, "/")
-            elseif urlindex and string.find(urlindex, "http.://") then
-                newurl = urlindex
-            else
-                newurl = key
-            end
-
-            self:Fetch(newurl, onFetchReceive, onFailure)
-        end, onFailure)
-    end
+        self:Fetch(newurl, onFetchReceive, onFailure)
+    end, onFailure)
 end
