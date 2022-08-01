a861ba022294d2939d176efdf1110dc290d9ea76
BananaInk <100006271+BananaInk@users.noreply.github.com>
2022-07-31 21:55:35 -0600

    Update cl_devtools.lua (#369)
    * Update cl_devtools.lua
    
    Added steam ID for VR tweaking
    
    * Update cl_devtools.lua

--- lua/swamp/dev/cl_devtools.lua
+++ lua/swamp/dev/cl_devtools.lua
@@ -13,7 +13,8 @@ local allowed = {
     ["STEAM_0:1:43528204"] = true, -- noz
     ["STEAM_0:0:44814758"] = true, -- brian
     ["STEAM_0:1:94321246"] = true, -- brittan
-    ["STEAM_0:1:38369552"] = true -- smor
+    ["STEAM_0:1:38369552"] = true, -- smor
+    ["STEAM_0:0:39563158"] = true -- bananainc
     
 }
 
