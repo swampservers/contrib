88d416c88d0207ddfe484431618d5322293968c4
Swamp Servers <swampservers@gmail.com>
2022-08-04 11:11:20 -0600

    glualint

--- lua/swamp/sh_init.lua
+++ lua/swamp/sh_init.lua
@@ -385,7 +385,7 @@ WHITE = Color(255, 255, 255, 255)
 --     end
 -- end
 function basememo(func, params)
-    local init,meta = {},{
+    local init, meta = {}, {
         __index = function(tab, key)
             local d, out = func(key)
 
@@ -400,9 +400,12 @@ function basememo(func, params)
     }
 
     if params then
-        for k,v in pairs(params) do meta[k]=v end
+        for k, v in pairs(params) do
+            meta[k] = v
+        end
+
         if params.init then
-            init,meta.init = params.init,nil
+            init, meta.init = params.init, nil
         end
     end
 
@@ -440,20 +443,22 @@ end
 -- If the function returns nil, nothing will be stored, and the second return value will be returned by the indexing.
 function memo(func, params)
     local limit = debug.getinfo(func, "u").nparams
-    assert(params==nil or params.init==nil or limit<=1, "init only for single argument memo")
+    assert(params == nil or params.init == nil or limit <= 1, "init only for single argument memo")
     local the_memo = limit <= 1 and basememo(func, params) or multimemo(func, params, {}, limit)
     -- getmetatable(the_memo).__call = function()
 
     return the_memo
 end
 
+local weakrefmeta = {
+    __mode = "v",
+    __call = function(t) return t[1] end
+}
 
-local weakrefmeta={__mode="v",__call=function(t) return t[1] end}
 --- weak reference, call it to get the thing or nil if its gone
 function weakref(value)
-    return setmetatable({value},weakrefmeta)
+    return setmetatable({value}, weakrefmeta)
 end
- 
 
 -- local test = memo(function(a,b,c) return a + b * c end)
 -- print(test[1][2][3])
