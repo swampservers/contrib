47193ea0fe27a57f88e799d58abb644ed955b786
Metroid55 <ssmetroitd@googlemail.com>
2023-02-08 03:05:24 +0100

    forgot to change the ump45 price

--- lua/cinema/guns/weapons/gun_ump45.lua
+++ lua/cinema/guns/weapons/gun_ump45.lua
@@ -39,7 +39,7 @@ SWEP.KickLBase = 0.2
 SWEP.KickLSpray = 0.3
 SWEP.MoveSpeed = 240 / 250
 --
-SWEP.SpawnPriceMod = 1.25 -- 23 / 18
+SWEP.SpawnPriceMod = 0.94 -- (23 / 0.102) / (18 / 0.075)
 SWEP.AmmoPriceMod = 25 / 30
 -- SWEP.KickMoving = {0.45, 0.3, 0.2, 0.0275, 4, 2.25, 7}
 -- SWEP.KickStanding = {0.3, 0.225, 0.125, 0.02, 3.25, 1.25, 8}
