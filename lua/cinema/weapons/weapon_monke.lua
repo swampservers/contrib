fcf7afcada44d90b2d5842cda3b567a8f06f73c1
Swamp Servers <swampservers@gmail.com>
2023-05-24 18:29:28 -0600

    Lint

--- lua/cinema/weapons/weapon_monke.lua
+++ lua/cinema/weapons/weapon_monke.lua
@@ -1,5 +1,4 @@
 ﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
-
 SWEP.PrintName = "Return To Monke"
 SWEP.Author = "PYROTEKNIK"
 SWEP.Category = "PYROTEKNIK"
