dde05497205e29ed5e9da0b3d7ee92bf59acdb33
swamproyal <139602367+swamproyal@users.noreply.github.com>
2023-07-30 15:18:27 -0500

    Start kick.com support

---
+++ lua/cinema/theater/services/sv_kick.lua
@@ -0,0 +1,21 @@
+﻿-- This file is subject to copyright - contact swampservers@gmail.com for more information.
+sv_GetVideoInfo = sv_GetVideoInfo or {}
+
+sv_GetVideoInfo.kick = function(self, key, ply, onSuccess, onFailure)
+    local onReceive = function(body, length, headers, code)
+        local t = util.JSONToTable(body)
+
+        if type(t) == "table" and t["livestream"]["session_title"] ~= nil and t["livestream"]["thumbnail"]["url"] ~= nil then
+            local info = {}
+            info.title = t["livestream"]["session_title"]
+            info.thumb = t["livestream"]["thumbnail"]["url"]
+            info.duration = 0
+
+            onSuccess(info)
+        else
+            onFailure('Theater_RequestFailed')
+        end
+    end
+
+    self:Fetch("https://kick.com/api/v2/channels/" .. key, onReceive, onFailure)
+end
