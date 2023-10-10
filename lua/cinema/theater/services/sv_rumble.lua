2ebacca6bfd133b7d3f61dbf9f6b0588fff1549c
swamproyal <139602367+swamproyal@users.noreply.github.com>
2023-10-09 22:10:18 -0500

    Add rumble.com support (#411)

---
+++ lua/cinema/theater/services/sv_rumble.lua
@@ -0,0 +1,6 @@
+-- This file is subject to copyright - contact swampservers@gmail.com for more information.
+sv_GetVideoInfo = sv_GetVideoInfo or {}
+
+sv_GetVideoInfo.rumble = function(self, key, ply, onSuccess, onFailure)
+    theater.GetVideoInfoClientside(self:GetClass(), key, ply, onSuccess, onFailure)
+end
