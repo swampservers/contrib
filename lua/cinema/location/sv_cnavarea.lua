7d3fd2de296298f8289b015f93ad7406ec0fbdc5
WinterPhoenix <winterphoenix96@protonmail.com>
2024-01-30 23:02:46 -0500

    Add our CNavArea:(Set/Get)Place override to contrib (used by Skeletons)

---
+++ lua/cinema/location/sv_cnavarea.lua
@@ -0,0 +1,21 @@
+﻿-- CNavArea:GetPlace override to use the Location system instead of its own internal thing
+local CNAVAREA = FindMetaTable("CNavArea")
+local CachedLocationNames = {}
+
+function CNAVAREA:SetPlace(...)
+    ErrorNoHalt("CNavArea:SetPlace is not supported. Use the Location system instead\n")
+
+    return false
+end
+
+function CNAVAREA:GetPlace()
+    local id = self:GetID()
+
+    if not CachedLocationNames[id] then
+        local loc_id = FindLocation(self:GetCenter())
+        local loc_info = Locations[loc_id]
+        CachedLocationNames[id] = loc_info.Name
+    end
+
+    return CachedLocationNames[id]
+end
