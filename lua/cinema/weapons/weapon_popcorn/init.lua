767edd46ada25bfa4c023f59e7905d4afb107ed0
Swamp Servers <swampservers@gmail.com>
2024-06-04 21:07:53 -0600

    Garry's Mod

--- lua/cinema/weapons/weapon_popcorn/init.lua
+++ lua/cinema/weapons/weapon_popcorn/init.lua
@@ -4,15 +4,15 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 util.AddNetworkString("EatPopcorn")
 
+local nospamlocations = {
+    ["Movie Theater"] = true,
+    ["Trump Tower Casino"] = true
+}
+
 function SWEP:SecondaryAttack()
     local bucket, att, phys, tr
-    self.Weapon:SetNextSecondaryFire(CurTime() + 0.15)
-
-    if self:GetClass() == "weapon_popcorn_spam" then
-        local t = self.Owner:GetTheater()
-        if t and t:Name() == "Movie Theater" then return end
-    end
-
+    self:SetNextSecondaryFire(CurTime() + 0.15)
+    if self:GetClass() == "weapon_popcorn_spam" and nospamlocations[self.Owner:GetLocationName()] then return end
     self.Owner:EmitSound("weapons/slam/throw.wav")
     self.Owner:ViewPunch(Angle(math.Rand(-8, 8), math.Rand(-8, 8), 0))
     bucket = ents.Create("ent_popcorn_thrown")
