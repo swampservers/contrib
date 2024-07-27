68e66e57e84b4d541f840a83c9d1b6b5e31de850
swamproyal <139602367+swamproyal@users.noreply.github.com>
2024-07-26 23:41:04 -0500

    Rumble fixes

--- lua/cinema/theater/services/sh_rumble.lua
+++ lua/cinema/theater/services/sh_rumble.lua
@@ -3,6 +3,48 @@ local SERVICE = {}
 SERVICE.Name = "Rumble"
 SERVICE.NeedsCodecs = true
 SERVICE.NeedsChromium = true
+SERVICE.ServiceJS = [[
+    var rumblePlayer;
+    var targetTime = -0.5;
+    var updateTimeNow = false;
+    var lastLiveClickTime = 0;
+    function th_volume(vol) {
+        if (rumblePlayer) {
+            rumblePlayer.volume = vol * 0.01;
+        }
+    }
+    function th_seek(seconds) {
+        targetTime = seconds - 0.5;
+        updateTimeNow = true;
+    }
+    // Think
+    setInterval(function() {
+        document.querySelector('.bigPlayUI')?.click();
+
+        targetTime += 0.1;
+        if (rumblePlayer) {
+            if (rumblePlayer.paused) {
+                rumblePlayer.play();
+            }
+            let liveButton = [...document.getElementsByTagName('span')].find(span => span.innerText === "● LIVE");
+            if (liveButton) {
+                targetTime = rumblePlayer.duration - 0.5;
+            }
+
+            var maxOffset = 15;
+            if (updateTimeNow || Math.abs(rumblePlayer.currentTime - targetTime) > maxOffset) {
+                rumblePlayer.currentTime = Math.max(0, targetTime);
+                updateTimeNow = false;
+            }
+        } else {
+            let player = document.querySelector('video');
+            if (player) {
+                rumblePlayer = player;
+                gmod.loaded();
+            }
+        }
+    }, 100);
+]]
 
 function SERVICE:GetKey(url)
     if string.match(url.host or "", "rumble.com") and string.match(url.path or "", "^/v%w%w%w%w%w%w%-(.+)%.html$") then return url.encoded end
@@ -19,15 +61,16 @@ if CLIENT then
                 vpanel:Remove()
             end
 
-            vpanel = vgui.Create("DHTML", nil, "FileVPanel")
-            vpanel:SetSize(100, 100)
+            vpanel = vgui.Create("DHTML", nil, "RumblePanel")
+            vpanel:SetSize(500, 500)
             vpanel:SetAlpha(0)
             vpanel:SetMouseInputEnabled(false)
+            vpanel:SetKeyboardInputEnabled(false)
 
             timer.Simple(20, function()
                 if IsValid(vpanel) then
                     vpanel:Remove()
-                    print("Failed")
+                    print("Rumble request failed")
                     callback()
                 end
             end)
@@ -36,6 +79,7 @@ if CLIENT then
                 self:AddFunction("gmod", "onVideoInfoReady", function(newVideoInfo)
                     table.Merge(videoInfo, newVideoInfo)
 
+                    -- 'data' is the embed video ID, different from the ID in the key
                     if videoInfo.title and videoInfo.data and videoInfo.duration and videoInfo.thumb then
                         callback(videoInfo)
                         self:Remove()
@@ -54,21 +98,22 @@ if CLIENT then
 
                         const videoInfoJson = document.querySelector('script[type="application/ld+json"]').textContent;
                         const videoInfoJsonObject = JSON.parse(videoInfoJson);
-
                         const videoTitle = videoInfoJsonObject[0].name;
                         const thumbnailUrl = videoInfoJsonObject[0].thumbnailUrl;
                         const durationISO8601 = videoInfoJsonObject[0].duration;
                         const durationSeconds = iso8601ToSeconds(durationISO8601);
                         const embedUrl = videoInfoJsonObject[0].embedUrl;
-
-                        const videoInfo = {
-                            "title": videoTitle,
-                            "data": embedUrl,
-                            "duration": durationSeconds,
-                            "thumb": thumbnailUrl
-                        };
-
-                        gmod.onVideoInfoReady(videoInfo);
+                        const match = embedUrl.match("/embed\/([^\/?]+)/");
+                        if (match) {
+                            const embedId = match[1];
+                            const videoInfo = {
+                                "title": videoTitle,
+                                "data": embedId,
+                                "duration": durationSeconds,
+                                "thumb": thumbnailUrl
+                            };
+                            gmod.onVideoInfoReady(videoInfo);
+                        }
                     ]])
                 end
             end
@@ -82,38 +127,29 @@ if CLIENT then
     end
 
     function SERVICE:LoadVideo(Video, panel)
-        panel:EnsureURL(Video:Data())
-
+        panel:SetHTML([[
+            <html>
+                <head>
+                    <style>
+                        html, body { margin: 0; padding: 0; height: 100%; width: 100%; background: black; }
+                    </style>
+                </head>
+                <body>
+                    <script>!function(r,u,m,b,l,e){r._Rumble=b,r[b]||(r[b]=function(){(r[b]._=r[b]._||[]).push(arguments);if(r[b]._.length==1){l=u.createElement(m),e=u.getElementsByTagName(m)[0],l.async=1,l.src="https://rumble.com/embedJS/u4"+(arguments[1].video?'.'+arguments[1].video:'')+"/?url="+encodeURIComponent(location.href)+"&args="+encodeURIComponent(JSON.stringify([].slice.apply(arguments))),e.parentNode.insertBefore(l,e)}})}(window, document, "script", "Rumble");</script>
+                    <div id="rumblePlayer"></div>
+                </body>
+            </html>
+        ]])
         panel:AddFunction("gmod", "loaded", function()
-            self:SeekTo(CurTime() - Video:StartTime(), panel)
+            if Video:Duration() > 0 then
+                self:SeekTo(CurTime() - Video:StartTime(), panel)
+            end
             self:SetVolume(theater.GetVolume(), panel)
         end)
-
-        panel:QueueJavascript("document.querySelector('.bigPlayUI.ctp').click();")
-        panel:QueueJavascript([[
-            var rumblePlayer;
-            const initInterval = setInterval(function() {
-                let player = document.querySelector('video');
-                if (player) {
-                    player.volume = 0;
-                    if (player.readyState > 0) {
-                        rumblePlayer = player;
-                        gmod.loaded();
-                        clearInterval(initInterval);
-                    }
-                }
-            }, 100);
-            function th_volume(vol) {
-                if (rumblePlayer) {
-                    rumblePlayer.volume = vol * 0.01;
-                }
-            }
-            function th_seek(time) {
-                if (rumblePlayer) {
-                    rumblePlayer.currentTime = time;
-                }
-            }
-        ]])
+        panel:QueueJavascript(theater.TheaterJS)
+        panel:QueueJavascript(self.ServiceJS)
+        panel:QueueJavascript("Rumble('play', {'video':'" .. Video:Data() .. "','div':'rumblePlayer', 'opts':['force_ga_load', 'noads', 'norumbleads']});")
+        panel:QueueJavascript("document.body.style.overflow = 'hidden';")
     end
 end
 
